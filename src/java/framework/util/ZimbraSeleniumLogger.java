d422ba0fe1ea7fec01b03cf7742e33ec9e754ef5
Unknown Perforce User <abareli>
2010-07-12 03:03:44 -0500

    (split)bug:none - using LogManager to getLogger
    Copied from Perforce
     Change: 242134

--- src/java/framework/util/ZimbraSeleniumLogger.java
+++ src/java/framework/util/ZimbraSeleniumLogger.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 
 public class ZimbraSeleniumLogger {
 
-	public static Logger mLog = Logger.getLogger(new GetCurClass().getCurrentClass());
+	public static Logger mLog = LogManager.getLogger(new GetCurClass().getCurrentClass());
 
 	public static <T> void setmLog (Class <T> clazz) {
 		if(clazz!=null)
