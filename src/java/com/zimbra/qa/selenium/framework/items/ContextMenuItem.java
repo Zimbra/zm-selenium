0b2d98587318bbe9d93044368a2be3de1ecc7435
Unknown Perforce User <dieunguyen>
2011-01-24 14:23:23 -0600

    (split)bug:none optimize code
    Copied from Perforce
     Change: 271932

--- src/java/com/zimbra/qa/selenium/framework/items/ContextMenuItem.java
+++ src/java/com/zimbra/qa/selenium/framework/items/ContextMenuItem.java
@@ -13,10 +13,10 @@ public class ContextMenuItem {
 	//contact's context menu	
 	public static final ContextMenuItem C_CONTACT_SEARCH = new ContextMenuItem("zmi__Contacts__SEARCH","Find Emails From Contact","div[class='ImgSearch']","");	
 	public static final ContextMenuItem C_CONTACT_ADVANCED_SEARCH = new ContextMenuItem("zmi__Contacts__BROWSE","Advanced Search","div[class='ImgSearchBuilder']","");	
-	public static final ContextMenuItem C_CONTACT_NEW_EMAIL = new ContextMenuItem("zmi__Contacts__NEW_MESSAGE","New Email","div[class='ImgNewMessage']","[nm]");	
+	public static final ContextMenuItem C_CONTACT_NEW_EMAIL = new ContextMenuItem("zmi__Contacts__NEW_MESSAGE","New Email","div[class='ImgNewMessage']",":contains('[nm]')"); 	
 	public static final ContextMenuItem C_CONTACT_EDIT = new ContextMenuItem("zmi__Contacts__CONTACT","Edit Contact","div[class='ImgEdit']","");	
 	public static final ContextMenuItem C_CONTACT_FORWARD = new ContextMenuItem("zmi__Contacts__SEND_CONTACTS_IN_EMAIL","Forward Contact","div[class='ImgMsgStatusSent']","");	
-	public static final ContextMenuItem C_CONTACT_TAG = new ContextMenuItem("zmi__Contacts__TAG_MENU","Tag Contact","div[class='ImgTag']","css=div[class='ImgCascade']");	
+	public static final ContextMenuItem C_CONTACT_TAG = new ContextMenuItem("zmi__Contacts__TAG_MENU","Tag Contact","div[class='ImgTag']"," div[class='ImgCascade']");	
 	public static final ContextMenuItem C_CONTACT_DELETE = new ContextMenuItem("zmi__Contacts__DELETE","Delete","div[class='ImgDelete']","");
 	public static final ContextMenuItem C_CONTACT_MOVE = new ContextMenuItem("zmi__Contacts__MOVE","Move","div[class='ImgMoveToFolder']","");
 	public static final ContextMenuItem C_CONTACT_PRINT = new ContextMenuItem("zmi__Contacts__PRINT_CONTACT","Print","div[class='ImgPrint']","");
@@ -28,10 +28,11 @@ public class ContextMenuItem {
 	public static final ContextMenuItem C_SEPARATOR = new ContextMenuItem("css=div[id='DWT']","","","");
     
     
-    private static HashMap<String,ContextMenuItem> hm = new HashMap();
+    private static HashMap<String,ContextMenuItem> hm = new HashMap<String,ContextMenuItem>();
 
     static
     {
+    	
     	//use reflection to put pre-defined ContextMenuItem objects into the Hashmap 
     	Field[] fields= ContextMenuItem.class.getFields();
        
@@ -49,7 +50,6 @@ public class ContextMenuItem {
 	public final String image;
 	public final String text;
 	public final String shortcut;
-	public boolean enable;
 	
 	private ContextMenuItem (String locator,String text,String image,String shortcut) {
 		this.locator=locator;
@@ -61,41 +61,28 @@ public class ContextMenuItem {
 	
 	public static ContextMenuItem getContextMenuItem  (String locator)throws HarnessException {
 	   ContextMenuItem cmi=null;
-	   //?????
-	   //if (hm.containsKey(locator))
-		   
-	   for (String key : hm.keySet()) {
-		  if (key.equals(locator)) {
-		     cmi= hm.get(key);
-		    
-		     //TODO verify text, image, shortcut 
-			 /*   
-		     String cssLocator= "css=div[id='" + locator + "']";
-		     String xpathLocator = "xpath=//div[@id='" + locator + "']"; 
-		     
-		 	 if (! (ClientSessionFactory.session().selenium().isElementPresent(cssLocator + " " +cmi.image) ||
-		    		ClientSessionFactory.session().selenium().isElementPresent(cssLocator + " " +cmi.text) ||
-		    		ClientSessionFactory.session().selenium().isElementPresent(cssLocator + " " +cmi.shortcut) 
-		         )) {
-		    	 
-		     }
-		    	 
-		     //_left_icon ._title _dropdown
-		     //String contextItemMain      = typeLocator + "/table/tbody/tr["+ i +"]";
-		     //String contextItemText      = contextItemMain + "/td/div/ table/tbody/tr/td[3]";
-			 //String contextItemLocator   = contextItemMain +"/td/div";
-			 //get attribute
-			 //String contextItemImage     = contextItemMain + "/td/div/table/tbody/tr/td[2]";
-			 //String contextItemShortcut  = contextItemMain + "/td/div/table/tbody/tr/td[4]";
-              */
-		     break;
-		  }
-		  else if (locator.startsWith("DWT")){
-			  //TODO probably separator 
-			  cmi = C_SEPARATOR;
-			  break;
-		  }
+	   if (hm.containsKey(locator)) {
+		   cmi= hm.get(locator); 	
+		   String cssLocator= "css=td[id='" + locator ;
+		 
+		   //verify image, text, and shortcut 		   
+		   if (! (ClientSessionFactory.session().selenium().isElementPresent(cssLocator + "_left_icon" + "'] " +cmi.image) 
+		    	||	ClientSessionFactory.session().selenium().isElementPresent(cssLocator + "_title" + "']:contains('" +cmi.text + "')") 
+		    	||	ClientSessionFactory.session().selenium().isElementPresent(cssLocator + "_dropdown" +"']" +cmi.shortcut) 
+		         )) 
+		   {
+			   logger.debug("Cannot find either" + cssLocator + "_left_icon" + "'] " +cmi.image 
+					   + " \nOR " + cssLocator + "_title" + "']:contains('" +cmi.text + "')"  
+					   + " \nOR " + cssLocator + "_dropdown" +"']" +cmi.shortcut 			   	   
+			               );					    
+			   cmi= null; 	 
+		   } 
 	   }
+	   else if (locator.startsWith("DWT")){
+			  //most likely separator 
+		   cmi = C_SEPARATOR;			  
+	   }
+	  
 		
 	   if (cmi == null) {
 		   throw new HarnessException("cannot find context menu " + locator);
@@ -106,7 +93,7 @@ public class ContextMenuItem {
 	
 	public boolean isEnable() {
 		//TODO get visible attribute from Selenium 
-		return enable;
+		return true;
 	}
 		
 }
