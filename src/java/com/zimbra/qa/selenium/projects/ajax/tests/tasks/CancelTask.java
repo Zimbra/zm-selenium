3324ac1041874c36e5072cec29bd4353ab08e2f5
Unknown Perforce User <girishb>
2011-02-17 00:09:30 -0600

    (split)bug:none Modified CancelTask_01() with removing wait statments
    Copied from Perforce
     Change: 276830

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/CancelTask.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/CancelTask.java
@@ -3,19 +3,14 @@ package com.zimbra.qa.selenium.projects.ajax.tests.tasks;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
 
 import java.util.List;
-
 import org.testng.annotations.Test;
-
 import com.zimbra.qa.selenium.framework.items.TaskItem;
 import com.zimbra.qa.selenium.framework.ui.AbsDialog;
 import com.zimbra.qa.selenium.framework.ui.Button;
-import com.zimbra.qa.selenium.framework.util.GeneralUtility;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
-
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew;
-import com.zimbra.qa.selenium.projects.ajax.ui.tasks.PageTasks;
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew.Field;
 
 public class CancelTask extends AjaxCommonTest {
@@ -27,9 +22,12 @@ public class CancelTask extends AjaxCommonTest {
 		super.startingAccountPreferences = null;
 	}
 	/**
-	 * Note: While running CancelTask test through selenium and click on cancel button then
-	 * some times Warning dialog doesn't appear 
-	 * so i have used 2 wait statement and explicitly goes into Task folder.
+	 * Test Case: CancelTask_01
+	 * Open new Task 
+	 * Enter Subject and body
+	 * Press Cancel button
+	 * Waring dialog should pop up and press no
+	 * Task should not show in list 
 	 * @throws HarnessException
 	 */
 	@Test(description = "Cancel composing of new task through GUI", groups = { "functional" })
@@ -38,23 +36,18 @@ public class CancelTask extends AjaxCommonTest {
 		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
 		String body = "taskbody" + ZimbraSeleniumProperties.getUniqueString();
 
-		GeneralUtility.waitForElementPresent(app.zPageTasks,PageTasks.Locators.zTasksTab, 2000);
-		
-		app.zPageTasks.zClick(PageTasks.Locators.zTasksTab);
-		
-		GeneralUtility.waitForElementPresent(app.zPageTasks,PageTasks.Locators.zNewTask, 20000);
 		FormTaskNew taskNew = (FormTaskNew) app.zPageTasks.zToolbarPressButton(Button.B_NEW);
-		
+
 		taskNew.zFillField(Field.Subject, subject);
 		taskNew.zFillField(Field.Body, body);
 
 		AbsDialog warning = (AbsDialog) taskNew.zToolbarPressButton(Button.B_CANCEL);
 		ZAssert.assertNotNull(warning, "Verify the dialog is returned");
-		
+
 		warning.zClickButton(Button.B_NO);
-		
+
 		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
-		
+
 		TaskItem found = null;
 		for (TaskItem t : tasks) {
 			logger.info("Subject: looking for " + subject + " found: "
@@ -64,8 +57,8 @@ public class CancelTask extends AjaxCommonTest {
 				break;
 			}
 		}
-		
+
 		ZAssert.assertNull(found, "Verify the task is no longer present in task list");
-		
+
 	}
 }
