147ca3d6c754b9e2acff7ce83f18c41d9039358b
Quanah Gibson-Mount <quanah@zimbra.com>
2011-09-16 13:12:42 -0500

    (split)bug: 55432 Updated license
    http://bugzilla.zimbra.com/show_bug.cgi?id=55432
    
    Copied from Perforce
     Change: 330733

--- zmBrowserPerf/zmBrowserPerfSel/src/core/lib/prototype.js
+++ zmBrowserPerf/zmBrowserPerfSel/src/core/lib/prototype.js
@@ -1,3 +1,19 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2010, 2011 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 /*  Prototype JavaScript framework, version 1.5.0_rc0
  *  (c) 2005 Sam Stephenson <sam@conio.net>
  *
