04c982397c243f392d500a2520ab991d17a51dbe
Unknown Perforce User <jhidayat>
2011-09-30 14:15:57 -0500

    (split)ZDesktop Selenium: Added legal headnotes for just-added briefcase.folders's CreateFolder and DeleteFolder, and also change import from ajax to desktop to allow ZDesktop automation to run. Bug: None
    Copied from Perforce
     Change: 332797

--- src/java/com/zimbra/qa/selenium/projects/desktop/tests/briefcase/folders/CreateFolder.java
+++ src/java/com/zimbra/qa/selenium/projects/desktop/tests/briefcase/folders/CreateFolder.java
@@ -1,3 +1,19 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 package com.zimbra.qa.selenium.projects.desktop.tests.briefcase.folders;
 
 import org.testng.annotations.*;
@@ -5,8 +21,8 @@ import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
-import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
-import com.zimbra.qa.selenium.projects.ajax.ui.briefcase.DialogCreateBriefcaseFolder;
+import com.zimbra.qa.selenium.projects.desktop.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.desktop.ui.briefcase.DialogCreateBriefcaseFolder;
 
 public class CreateFolder extends AjaxCommonTest {
 
@@ -138,8 +154,8 @@ public class CreateFolder extends AjaxCommonTest {
 		_folderName = "folder" + ZimbraSeleniumProperties.getUniqueString();
 
 		// Create a new briefcase folder using right click context menu + New Briefcase
-		DialogCreateBriefcaseFolder dialog = (DialogCreateBriefcaseFolder) app.zPageBriefcase
-				.zToolbarPressPulldown(Button.B_NEW, Button.O_NEW_BRIEFCASE, null);
+		DialogCreateBriefcaseFolder dialog = (DialogCreateBriefcaseFolder) app.zTreeBriefcase
+				.zTreeItem(Action.A_RIGHTCLICK, Button.B_TREE_NEWFOLDER,briefcaseRootFolder);
 
 		ZAssert.assertNotNull(dialog, "Verify the new dialog opened");
 
