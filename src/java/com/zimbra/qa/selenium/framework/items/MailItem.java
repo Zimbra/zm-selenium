bf87be3d5de8cff84d8ad42599d2ae308eb28bd5
Unknown Perforce User <rhoades>
2013-10-25 14:49:56 -0500

    (split)Bug: none
    Selenium Harness: Remove references to SOAP_DESTINATION_HOST_TYPE.* in items
    
    Copied from Perforce
     Change: 487179

--- src/java/com/zimbra/qa/selenium/framework/items/MailItem.java
+++ src/java/com/zimbra/qa/selenium/framework/items/MailItem.java
@@ -29,7 +29,6 @@ import com.zimbra.common.soap.Element;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.XmlStringUtil;
 import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
-import com.zimbra.qa.selenium.framework.util.ZimbraAccount.SOAP_DESTINATION_HOST_TYPE;
 
 
 /**
@@ -330,21 +329,12 @@ public class MailItem implements IItem {
 
 	public static MailItem importFromSOAP(ZimbraAccount account, String query) throws HarnessException {
 		
-		return importFromSOAP(account, query, SOAP_DESTINATION_HOST_TYPE.SERVER, null);
-
-	}
-
-	public static MailItem importFromSOAP(ZimbraAccount account, String query,
-	      SOAP_DESTINATION_HOST_TYPE destType, String accountName) throws HarnessException {
-
 	   try {
 
          account.soapSend(
                "<SearchRequest xmlns='urn:zimbraMail' types='message'>" +
                   "<query>"+ query +"</query>" +
-               "</SearchRequest>",
-               destType,
-               accountName);
+               "</SearchRequest>");
          
          Element[] results = account.soapSelectNodes("//mail:SearchResponse/mail:m");
          if (results.length != 1)
@@ -356,9 +346,7 @@ public class MailItem implements IItem {
          account.soapSend(
                "<GetMsgRequest xmlns='urn:zimbraMail'>" +
                      "<m id='"+ id +"' />" +
-                   "</GetMsgRequest>",
-                   destType,
-                   accountName);
+                   "</GetMsgRequest>");
          Element getMsgResponse = account.soapSelectNode("//mail:GetMsgResponse", 1);
          
          // Using the response, create this item
