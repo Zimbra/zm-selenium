4c0125a38fad974643884d4106e0403e7ac3eb49
Jitesh Sojitra <jsojitra@zimbra.com>
2015-01-13 01:07:05 -0600

    (split)bug:96437,96860,95807 - Added 4 testcases for accept instance and series (Accept Instance, Accept Series, Decline Instance, Decline Series) - Added 6 testcases for view appointment (Accept Appointment, Decline Appointment, Tentative Appointment, Edit Appointment, Delete Appointment, Permanently Delete Appointment)
    PS: main branch integration would be done together for all touch client files so not needed individually for now.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=96437
    https://bugzilla.zimbra.com/show_bug.cgi?id=96860
    https://bugzilla.zimbra.com/show_bug.cgi?id=95807
    
    Copied from Perforce
     Change: 533776

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/calendar/appointments/dayview/attendee/viewappt/Accept.java
@@ -0,0 +1,136 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.calendar.appointments.dayview.attendee.viewappt;
+
+import java.util.*;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.*;
+
+public class Accept extends CalendarWorkWeekTest {
+
+	public Accept() {
+		logger.info("New "+ Accept.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+
+	@Bugs(ids = "95807")
+	@Test(	description = "Accept a meeting using view appointment options", 
+			groups = { "sanity" })
+	
+	public void AcceptMeeting_01() throws HarnessException {
+
+		// ------------------------ Test data ------------------------------------
+
+		String apptSubject = "appointment" + ZimbraSeleniumProperties.getUniqueString();
+
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+
+		// --------------- Creating invitation (organizer) ----------------------------
+
+		ZimbraAccount.AccountA().soapSend(
+				"<CreateAppointmentRequest xmlns='urn:zimbraMail'>"
+				+		"<m>"
+				+			"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"
+				+				"<s d='"+ startUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<e d='"+ endUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<or a='"+ ZimbraAccount.AccountA().EmailAddress +"'/>"
+				+				"<at role='REQ' ptst='NE' rsvp='1' a='" + app.zGetActiveAccount().EmailAddress + "'/>"
+				+			"</inv>"
+				+			"<e a='"+ app.zGetActiveAccount().EmailAddress +"' t='t'/>"
+				+			"<su>"+ apptSubject +"</su>"
+				+			"<mp content-type='text/plain'>"
+				+				"<content>content</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateAppointmentRequest>");
+		app.zPageCalendar.zRefresh();
+        app.zPageCalendar.zGoToToday(startUTC);
+        
+        // Select appointment and accept it
+        app.zPageCalendar.zListItem(Action.A_LEFTCLICK, Button.O_ACCEPT_MENU, apptSubject);
+
+        
+		// ---------------- Verification at organizer & invitee side both -------------------------------------
+
+		// --- Check that the organizer shows the attendee as "ACCEPT" ---
+
+		// Organizer: Search for the appointment (InvId)
+		ZimbraAccount.AccountA().soapSend(
+					"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+				+		"<query>"+ apptSubject +"</query>"
+				+	"</SearchRequest>");
+		
+		String organizerInvId = ZimbraAccount.AccountA().soapSelectValue("//mail:appt", "invId");
+
+		// Get the appointment details
+		ZimbraAccount.AccountA().soapSend(
+					"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ organizerInvId +"'/>");
+		
+		String attendeeStatus = ZimbraAccount.AccountA().soapSelectValue("//mail:at[@a='"+ app.zGetActiveAccount().EmailAddress +"']", "ptst");
+		//String attendeeStatus = ZimbraAccount.AccountA().soapSelectValue("//mail:reply[@at='"+ app.zGetActiveAccount().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=AC
+		ZAssert.assertEquals(attendeeStatus, "AC", "Verify that the attendee shows as 'ACCEPTED'");
+
+
+		// --- Check that the attendee showing status as "ACCEPT" ---
+
+		// Attendee: Search for the appointment (InvId)
+		app.zGetActiveAccount().soapSend(
+					"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+				+		"<query>"+ apptSubject +"</query>"
+				+	"</SearchRequest>");
+		
+		String attendeeInvId = app.zGetActiveAccount().soapSelectValue("//mail:appt", "invId");
+
+		// Get the appointment details
+		app.zGetActiveAccount().soapSend(
+					"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ attendeeInvId +"'/>");
+		
+		String myStatus = app.zGetActiveAccount().soapSelectValue("//mail:at[@a='"+ app.zGetActiveAccount().EmailAddress +"']", "ptst");
+		//String myStatus = ZimbraAccount.AccountA().soapSelectValue("//mail:reply[@at='"+ app.zGetActiveAccount().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=AC
+		ZAssert.assertEquals(myStatus, "AC", "Verify that the attendee shows as 'ACCEPTED'");
+		
+		// Organizer: Search for the appointment response
+		String inboxId = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox).getId();
+		
+		ZimbraAccount.AccountA().soapSend(
+					"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+				+		"<query>inid:"+ inboxId +" subject:("+ apptSubject +")</query>"
+				+	"</SearchRequest>");
+		
+		String messageId = ZimbraAccount.AccountA().soapSelectValue("//mail:m", "id");
+
+		// Get the appointment details
+		ZimbraAccount.AccountA().soapSend(
+					"<GetMsgRequest  xmlns='urn:zimbraMail'>"
+				+		"<m id='"+ messageId +"'/>"
+				+	"</GetMsgRequest>");
+
+	}
+
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/calendar/appointments/dayview/attendee/viewappt/Edit.java
@@ -0,0 +1,86 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.calendar.appointments.dayview.attendee.viewappt;
+
+import java.util.*;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.*;
+import com.zimbra.qa.selenium.projects.touch.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.touch.ui.calendar.FormApptNew.Field;
+
+public class Edit extends CalendarWorkWeekTest {
+
+	public Edit() {
+		logger.info("New "+ Edit.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+
+	@Test(	description = "Edit meeting and locally save it", 
+			groups = { "functional" })
+	
+	public void EditMeeting_01() throws HarnessException {
+
+		// ------------------------ Test data ------------------------------------
+
+		String apptSubject = "appointment" + ZimbraSeleniumProperties.getUniqueString();
+		String modifiedApptSubject = ZimbraSeleniumProperties.getUniqueString();
+        String modifiedApptBody = ZimbraSeleniumProperties.getUniqueString();
+
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+
+		// --------------- Creating invitation (organizer) ----------------------------
+
+		ZimbraAccount.AccountA().soapSend(
+				"<CreateAppointmentRequest xmlns='urn:zimbraMail'>"
+				+		"<m>"
+				+			"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"
+				+				"<s d='"+ startUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<e d='"+ endUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<or a='"+ ZimbraAccount.AccountA().EmailAddress +"'/>"
+				+				"<at role='REQ' ptst='NE' rsvp='1' a='" + app.zGetActiveAccount().EmailAddress + "'/>"
+				+			"</inv>"
+				+			"<e a='"+ app.zGetActiveAccount().EmailAddress +"' t='t'/>"
+				+			"<su>"+ apptSubject +"</su>"
+				+			"<mp content-type='text/plain'>"
+				+				"<content>content</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateAppointmentRequest>");
+		app.zPageCalendar.zRefresh();
+        app.zPageCalendar.zGoToToday(startUTC);
+        
+        // Select appointment and modify it locally
+        FormApptNew apptForm = (FormApptNew)app.zPageCalendar.zListItem(Action.A_LEFTCLICK, Button.B_EDIT, apptSubject);
+        apptForm.zFillField(Field.Subject, modifiedApptSubject);
+        apptForm.zFillField(Field.Body, modifiedApptBody);
+        apptForm.zSubmit();
+        
+        // Use GetAppointmentRequest to verify the changes are saved
+        AppointmentItem modifyAppt = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ modifiedApptSubject +")");
+        ZAssert.assertNotNull(modifyAppt, "Verify the modified appointment appears on the server");
+        
+        ZAssert.assertEquals(modifyAppt.getSubject(), modifiedApptSubject, "Subject: Verify modified appointment subject");
+        ZAssert.assertStringContains(modifyAppt.getContent(), modifiedApptBody, "Body: Verify modified appointment body");
+	}
+}
