1d034b9b84c41f31d1c98461e17e19fae1d41701
Nidhi Vyas <nidhi@zimbra.com>
2015-01-30 05:14:43 -0600

    (split)bug:96864,97086 Added new test-cases com.zimbra.qa.selenium.projects.ajax.tests.calendar.bugs.Bug60134.Bug60134_01 com.zimbra.qa.selenium.projects.ajax.tests.calendar.bugs.Bug27661.Bug27661_01 com.zimbra.qa.selenium.projects.ajax.tests.calendar.bugs.Bug52682.Bug52682_01
    https://bugzilla.zimbra.com/show_bug.cgi?id=96864
    https://bugzilla.zimbra.com/show_bug.cgi?id=97086
    
    Copied from Perforce
     Change: 535669

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/bugs/Bug52682.java
@@ -0,0 +1,115 @@
+/*
+* ***** BEGIN LICENSE BLOCK *****
+* Zimbra Collaboration Suite Server
+* Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+*
+* This program is free software: you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software Foundation,
+* version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+* ***** END LICENSE BLOCK *****
+*/
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.bugs;
+
+import java.util.Calendar;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.DialogOpenRecurringItem;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew.Field;
+
+public class Bug52682 extends CalendarWorkWeekTest {
+
+public Bug52682() {
+logger.info("New "+ Bug52682.class.getCanonicalName());
+
+super.startingPage = app.zPageCalendar;
+}
+
+@Bugs(ids = "52682")
+@Test(
+description = "Can't change location of one instance",
+groups = { "functional" }
+)
+public void Bug52682_01() throws HarnessException {
+	// Create a meeting
+	ZimbraResource location = new ZimbraResource(ZimbraResource.Type.LOCATION);
+	
+	String tz = ZTimeZone.TimeZoneEST.getID();
+	String apptSubject = ZimbraSeleniumProperties.getUniqueString();
+	String apptAttendee = ZimbraAccount.AccountA().EmailAddress;
+	String apptLocation = location.EmailAddress;
+	
+	// Absolute dates in UTC zone
+	Calendar now = this.calendarWeekDayUTC;
+	ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+	ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+	
+	app.zGetActiveAccount().soapSend(
+            "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                 "<m>"+
+                 	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                 		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                 		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                 		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                 		"<at role='REQ' ptst='AC' rsvp='1' a='" + apptAttendee + "' d='2'/>" +
+						"<recur>" +
+						"<add>" +
+							"<rule freq='DAI'>" +
+								"<interval ival='1'/>" +
+							"</rule>" +
+						"</add>" +
+					"</recur>" +
+               	"</inv>" +
+                 	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                 	"<mp content-type='text/plain'>" +
+                 		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                 	"</mp>" +
+                 "<su>"+ apptSubject +"</su>" +
+                 "</m>" +
+           "</CreateAppointmentRequest>");
+    app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+    
+	// --------------- Modify first instance by adding location ----------------------------------------------------
+	DialogOpenRecurringItem openRecurring = (DialogOpenRecurringItem) app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+	openRecurring.zClickButton(Button.B_OPEN_THIS_INSTANCE);
+	openRecurring.zClickButton(Button.B_OK);
+
+	FormApptNew apptForm = new FormApptNew(app);
+	apptForm.zVerifyDisabledControlInOpenInstance();
+    apptForm.zFillField(Field.Location, apptLocation);
+    apptForm.zToolbarPressButton(Button.B_SEND);
+    SleepUtil.sleepMedium();; 
+
+    // Verify location in the appointment
+    
+	// Organizer: Search for the appointment
+    app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+			+		"<query>"+ apptSubject +"</query>"
+			+	"</SearchRequest>");
+    
+	String exceptionLocation = app.zGetActiveAccount().soapSelectValue("//mail:inst[@ex='1']", "loc");
+   
+	ZAssert.assertEquals(exceptionLocation, apptLocation, "Location: Verify the appointment data");
+	
+	// Verify location free/busy status is "Accepted"
+	String locationStatus = app.zGetActiveAccount().soapSelectValue("//mail:inst[@ex='1']", "ptst");
+	ZAssert.assertEquals(locationStatus, "AC", "Verify location status shows accepted");
+
+
+}
+
+}
+
