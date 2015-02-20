250ad448c6c066417ed060c4c14547f3242b6471
Pallavi Khairnar <pkhairnar@zimbra.com>
2015-02-20 02:42:29 -0600

    (split)bug: 97762 1. Added test cases to verify add, delete, rename operations for tags. 2. Updated Files: PageCreateTag 4. Added Testcases: CreateTag_01, CreateTag_02, DeleteTag_01, DeleteTag_02, RenameTag_01
    https://bugzilla.zimbra.com/show_bug.cgi?id=97762
    
    Copied from Perforce
     Change: 537522

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/contacts/tags/CreateTag.java
@@ -0,0 +1,129 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.qa.selenium.projects.touch.tests.contacts.tags;
+
+import org.testng.annotations.*;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.ui.AppTouchClient;
+import com.zimbra.qa.selenium.projects.touch.ui.PageCreateTag;
+import com.zimbra.qa.selenium.projects.touch.core.TouchCommonTest;
+
+public class CreateTag extends TouchCommonTest {
+
+	private String tagName = null;
+	private String errorMessage = null;
+	public CreateTag() {
+		logger.info("New " + CreateTag.class.getCanonicalName());
+		super.startingPage = app.zPageAddressbook;
+	}
+	
+	/**
+	 * Test case : Create a tag item
+	 * Steps :
+	 * 1. Create a tag from GUI. 
+	 * 2. Verify tag is created from SOAP & UI
+	 * @throws HarnessException
+	 */
+	
+	@Test( description = "Create a new tag by clicking to Edit -> 'New tag'", 
+			groups = { "sanity" })
+	
+	public void CreateTag_01() throws HarnessException  {
+		
+		tagName = "tag" + ZimbraSeleniumProperties.getUniqueString();
+	
+		PageCreateTag createTagPage = new PageCreateTag((AppTouchClient) app , this.startingPage);
+		
+		//Click on edit button
+		createTagPage.zClickButton(Button.B_EDIT);
+		//Click on new tag button
+		createTagPage.zClickButton(Button.B_NEW_TAG);
+		//Enter tag name
+		createTagPage.zEnterTagName(tagName);
+		//Click on Save
+		createTagPage.zClickButton(Button.B_SAVE);
+
+		// Make sure the tag was created on the ZCS server
+		TagItem tag = TagItem.importFromSOAP(app.zGetActiveAccount(),tagName);
+		ZAssert.assertNotNull(tag, "Verify the new tag was created");
+		ZAssert.assertEquals(tag.getName(), tagName,"Verify the server and client tag names match");
+		
+		// UI verification
+		ZAssert.assertTrue(createTagPage.zVerifyTagExists(tagName), "Verify created tag visible in tag pane");		
+	}
+	
+	/**
+	 * Test case : Verify error is displayed on creation of duplicate tag 
+	 * Steps :
+	 * 1. Create a tag from GUI.
+	 * 2. Try to create duplicate tag 
+	 * 3. Verify error message
+	 * 4. Verify existing tag
+	 * @throws HarnessException
+	 */
+	
+	@Test( description = "Try to create duplicate tag & verify error", 
+			groups = { "functional" })
+	
+	public void CreateTag_02() throws HarnessException  {
+		
+		tagName = "tag" + ZimbraSeleniumProperties.getUniqueString();
+		errorMessage="An object with that name already exists.";
+	
+		PageCreateTag createTagPage = new PageCreateTag((AppTouchClient) app , this.startingPage);
+		
+		//Click on edit button
+		createTagPage.zClickButton(Button.B_EDIT);
+		//Click on new tag button
+		createTagPage.zClickButton(Button.B_NEW_TAG);
+		//Enter tag name
+		createTagPage.zEnterTagName(tagName);
+		//Click on Save
+		createTagPage.zClickButton(Button.B_SAVE);
+		
+		//Try to create duplicate tag
+		//Click on edit button
+		createTagPage.zClickButton(Button.B_EDIT);
+		//Click on new tag button
+		createTagPage.zClickButton(Button.B_NEW_TAG);
+		//Enter tag name
+		createTagPage.zEnterTagName(tagName);
+		//Click on Save
+		createTagPage.zClickButton(Button.B_SAVE);
+		
+		
+		//Verify error dialog is displayed
+		createTagPage.zVerifyDialogText(errorMessage);
+		
+		//Click on OK button 
+		createTagPage.zClickButton(Button.B_OK);
+		
+		// Verify existing tag
+		TagItem tag = TagItem.importFromSOAP(app.zGetActiveAccount(),tagName);
+		ZAssert.assertNotNull(tag, "Verify the new tag was created");
+		ZAssert.assertEquals(tag.getName(), tagName,"Verify the server and client tag names match");
+				
+		// UI verification
+		ZAssert.assertTrue(createTagPage.zVerifyTagExists(tagName), "Verify created tag visible in tag pane");
+		
+		
+	}
+		
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/contacts/tags/RenameTag.java
@@ -0,0 +1,76 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.contacts.tags;
+
+import org.testng.annotations.*;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.ui.AppTouchClient;
+import com.zimbra.qa.selenium.projects.touch.ui.PageCreateTag;
+import com.zimbra.qa.selenium.projects.touch.core.TouchCommonTest;
+
+public class RenameTag extends TouchCommonTest  {
+	public RenameTag() {
+			logger.info("New "+ RenameTag.class.getCanonicalName());
+	}
+	
+	/**
+	 * Test case : rename a tag item.
+	 * Steps :
+	 * 1. Create a tag item using SOAP.
+	 * 2. rename tag from GUI. 
+	 * 3. Verify tag is renamed using SOAP.
+	 * @throws HarnessException
+	 */
+	
+	@Test(	description = "Rename a tag item", 
+			groups = { "smoke" })
+	
+	public void RenameTag_01() throws HarnessException {
+		//----------------------- Data ------------------------------
+		// Create the tag to rename
+		TagItem tag = TagItem.CreateUsingSoap(app.zGetActiveAccount());
+		ZAssert.assertNotNull(tag, "Verify the tag was created");
+		
+		String tagName = tag.getName();
+		String newTagName= "Tag" + ZimbraSeleniumProperties.getUniqueString();;
+		
+		// Refresh to get the tag into the client
+		app.zPageMail.zRefresh();		
+		SleepUtil.sleepSmall();
+		
+		PageCreateTag createTagPage = new PageCreateTag((AppTouchClient) app , this.startingPage);
+		
+		//Click on edit button
+		createTagPage.zClickButton(Button.B_EDIT);
+		
+		//Select tag
+		createTagPage.zSelectTag(tagName);
+		
+		// Rename the tag
+		createTagPage.zEnterTagName(newTagName);
+	
+		//Click on Save
+		createTagPage.zClickButton(Button.B_SAVE);
+			
+		// UI verification
+		ZAssert.assertTrue(createTagPage.zVerifyTagExists(newTagName), "Verify created tag visible in tag pane");
+	}
+	
+}
+
