50acfdea576ba18f1e8411d09ea57d967ae82f71
Girish Bhamare <girish@zimbra.com>
2015-03-09 07:06:10 -0500

    (split)bug:97591
    --Added test case for Right click To attendee bubble address>>Verify Delete/Copy/Edit/Expand/AddtoContacts menus
    --Right click To attendee bubble address>>Delete functionality
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97591
    
    Copied from Perforce
     Change: 539008

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/addresscontextmenu/AddressContextMenuOptions.java
@@ -0,0 +1,45 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.addresscontextmenu;
+
+//package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+
+
+public class AddressContextMenuOptions extends PrefGroupMailByMessageTest {
+
+	public AddressContextMenuOptions() {
+		logger.info("New " + AddressContextMenuOptions.class.getCanonicalName());
+
+		super.startingPage = app.zPageCalendar;
+
+	}
+
+	@Test(description = "Right click To attendee bubble address>>Verify Delete/Copy/Edit/Expand/AddtoContacts menus", groups = { "sanity" })
+	public void VerifyAttendeesContextMenuOptions() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+		
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		app.zPageCalendar.zRightClickAddressBubble();
+		logger.info(app.zPageMail.zVerifyAllAddressContextMenu("calendar"));
+		ZAssert.assertTrue(
+				app.zPageMail.zVerifyAllAddressContextMenu("calendar"),
+				"Delete/Copy/Edit/Expand/AddtoContact menu should be exist");
+
+	}
+
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/addresscontextmenu/DeleteAddressContextMenu.java
@@ -0,0 +1,118 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.addresscontextmenu;
+
+//package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning.DialogWarningID;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.PageCalendar.Locators;
+
+
+public class DeleteAddressContextMenu extends PrefGroupMailByMessageTest {
+
+	public DeleteAddressContextMenu() {
+		logger.info("New " + DeleteAddressContextMenu.class.getCanonicalName());
+
+		super.startingPage = app.zPageCalendar;
+
+	}
+
+	@Test(description = "Right click To bubble address>>Delete", groups = { "smoke" })
+	public void DeleteAttendeeContextMenu() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		app.zPageCalendar.zRightClickAddressBubble();
+		app.zPageMail.DeleteAddressContextMenu();
+
+		ZAssert.assertTrue(app.zPageMail.sIsElementPresent("css=td[id='zcs1_person'] div div[class='addrBubbleHolder-empty']"),"Attendee should be empty");
+		app.zPageCalendar.zClickAt(Locators.CloseAptTab,"");
+
+		DialogWarning warning = (DialogWarning) app.zPageMain.zGetWarningDialog(DialogWarningID.SaveChanges);
+		if ( warning.zIsActive() ) {
+			warning.zClickButton(Button.B_NO);
+		}
+
+
+
+	}
+
+	@Test(description = "Right click To bubble address>>Delete (Keyboard shortcut)", groups = { "smoke" })
+	public void DeleteAttendeeContextMenu_DeleteShortcut() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		app.zPageCalendar.sMouseOut(Locators.AttendeeBubbleAddr);
+		app.zPageCalendar.sClickAt(Locators.AttendeeBubbleAddr,"");
+		app.zPageCalendar.sKeyDown(Locators.AttendeeBubbleAddr, "46");
+		ZAssert.assertTrue(app.zPageMail.sIsElementPresent("css=td[id='zcs1_person'] div div[class='addrBubbleHolder-empty']"),"Attendee should be empty");
+		app.zPageCalendar.zClickAt(Locators.CloseAptTab,"");
+
+		DialogWarning warning = (DialogWarning) app.zPageMain.zGetWarningDialog(DialogWarningID.SaveChanges);
+		if ( warning.zIsActive() ) {
+			warning.zClickButton(Button.B_NO);
+		}
+
+	}
+
+	@Test(description = "Right click To bubble address>>Delete (BackSpace shortcut)", groups = { "smoke" })
+	public void DeleteAttendeeContextMenu_Backspace() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		app.zPageCalendar.sMouseOut(Locators.AttendeeBubbleAddr);
+		//	app.zPageCalendar.sMouseOver(Locators.AttendeeBubbleAddr);
+		app.zPageCalendar.sClickAt(Locators.AttendeeBubbleAddr,"");
+
+		//Backspace delete keycode=8
+		app.zPageCalendar.zKeyDown("8");
+		//app.zPageCalendar.sKeyDown(Locators.AttendeeBubbleAddr, "8");
+		ZAssert.assertTrue(app.zPageMail.sIsElementPresent("css=td[id='zcs1_person'] div div[class='addrBubbleHolder-empty']"),"Attendee should be empty");
+		app.zPageCalendar.zClickAt(Locators.CloseAptTab,"");
+
+		DialogWarning warning = (DialogWarning) app.zPageMain.zGetWarningDialog(DialogWarningID.SaveChanges);
+		if ( warning.zIsActive() ) {
+			warning.zClickButton(Button.B_NO);
+		}
+
+
+
+	}
+
+
+}
