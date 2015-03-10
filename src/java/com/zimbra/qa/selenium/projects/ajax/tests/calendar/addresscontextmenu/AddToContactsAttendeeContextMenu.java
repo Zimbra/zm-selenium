57afbc91ab4df0079573f76d0812b8e65d926d81
Girish Bhamare <girish@zimbra.com>
2015-03-10 04:42:31 -0500

    (split)bug:97591
    --Added test cases Right click To attendee bubble address>>Verify Edit menu
    --Added test case Right click To attendee bubble address>>Verify AddToContact menu
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97591
    
    Copied from Perforce
     Change: 539101

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/addresscontextmenu/AddToContactsAttendeeContextMenu.java
@@ -0,0 +1,80 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.addresscontextmenu;
+
+//package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.addressbook.FormContactNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.PageCalendar.Locators;
+
+
+public class AddToContactsAttendeeContextMenu extends PrefGroupMailByMessageTest {
+
+	public AddToContactsAttendeeContextMenu() {
+		logger.info("New " + AddToContactsAttendeeContextMenu.class.getCanonicalName());
+
+		super.startingPage = app.zPageCalendar;
+
+	}
+
+	@Test(description = "Right click To attendee bubble address>>Verify AddToContact", groups = { "smoke" })
+	public void AddToContactAttendeeContextMenu() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		String contactFirst = "First"+ ZimbraSeleniumProperties.getUniqueString();
+
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		String OriginalEmailAddr = app.zPageCalendar.sGetText(Locators.AttendeeBubbleAddr);
+
+		app.zPageCalendar.zRightClickAddressBubble();
+		app.zPageMail.AddToContactAddressContextMenu();
+
+		SleepUtil.sleepMedium();
+
+		app.zPageMail.sFocus("css=input[id$='_FIRST_input']");
+		app.zPageMail.sType("css=input[id$='_FIRST_input']", contactFirst);
+		app.zPageMail.sClickAt(FormContactNew.Toolbar.SAVE, "");
+		SleepUtil.sleepMedium();
+
+		// -- Data Verification
+
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='contact'>"
+						+ "<query>#firstname:" + contactFirst + "</query>"
+						+ "</SearchRequest>");
+		String contactId = app.zGetActiveAccount().soapSelectValue("//mail:cn",
+				"id");
+
+		ZAssert.assertNotNull(contactId,"Verify the contact is returned in the search");
+
+		app.zGetActiveAccount().soapSend(
+				"<GetContactsRequest xmlns='urn:zimbraMail'>" + "<cn id='"
+						+ contactId + "'/>" + "</GetContactsRequest>");
+
+		String firstname = app.zGetActiveAccount().soapSelectValue(
+				"//mail:cn[@id='" + contactId + "']//mail:a[@n='firstName']",
+				null);
+		String email = app.zGetActiveAccount().soapSelectValue(
+				"//mail:cn[@id='" + contactId + "']//mail:a[@n='email']", null);
+
+		ZAssert.assertEquals(firstname, contactFirst,"Verify the first name was saved correctly");
+		ZAssert.assertEquals(email, OriginalEmailAddr,"Verify the email was saved correctly");
+
+
+	}
+
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/addresscontextmenu/EditAttendeeContextMenu.java
@@ -0,0 +1,50 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.addresscontextmenu;
+
+//package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.PageCalendar.Locators;
+
+
+public class EditAttendeeContextMenu extends PrefGroupMailByMessageTest {
+
+	public EditAttendeeContextMenu() {
+		logger.info("New " + EditAttendeeContextMenu.class.getCanonicalName());
+
+		super.startingPage = app.zPageCalendar;
+
+	}
+
+	@Test(description = "Right click To attendee bubble address>>Verify Edit menus", groups = { "smoke" })
+	public void EditAttendeesContextMenu() throws HarnessException {
+
+		String apptAttendee1,apptContent;
+		AppointmentItem appt = new AppointmentItem();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		apptContent = ZimbraSeleniumProperties.getUniqueString();
+		appt.setAttendees(apptAttendee1);
+		appt.setContent(apptContent);
+
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar
+				.zToolbarPressButton(Button.B_NEW);
+		apptForm.zFill(appt);
+
+		app.zPageCalendar.zRightClickAddressBubble();
+		app.zPageMail.EditAddressContextMenu();
+
+		app.zPageCalendar.sFocus(FormApptNew.Locators.AttendeeField);
+		app.zPageCalendar.zClick(FormApptNew.Locators.AttendeeField);
+		app.zPageCalendar.zType(FormApptNew.Locators.AttendeeField,"test@test.com");
+		app.zPageCalendar.sKeyDown(FormApptNew.Locators.AttendeeField, "13");
+		SleepUtil.sleepMedium();
+		ZAssert.assertEquals(app.zPageCalendar.sGetText(Locators.AttendeeBubbleAddr), "test@test.com", "Edited address should present");	
+
+	}
+
+}
