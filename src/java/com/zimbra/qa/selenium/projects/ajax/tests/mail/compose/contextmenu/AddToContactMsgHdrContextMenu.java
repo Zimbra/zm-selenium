b5939ff1065b358f65a20dbb3fd42a3662d23e53
Girish Bhamare <girish@zimbra.com>
2015-03-13 03:19:30 -0500

    (split)bug:97591
    --Added  following test cases
    Receive a  mail - Right Click From Msg Header and verify context menus>>New Emails
    Receive a  mail - Right Click From Msg Header and verify context menus>>AddToContact
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97591
    
    Copied from Perforce
     Change: 539488

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/contextmenu/AddToContactMsgHdrContextMenu.java
@@ -0,0 +1,89 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.addressbook.FormContactNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
+
+
+public class AddToContactMsgHdrContextMenu extends PrefGroupMailByMessageTest {
+
+	public AddToContactMsgHdrContextMenu() {
+		logger.info("New "
+				+ AddToContactMsgHdrContextMenu.class.getCanonicalName());
+
+		super.startingAccountPreferences.put("zimbraPrefComposeFormat", "text");
+
+	}
+
+	@Test(description = "Receive a  mail - Right Click From Msg Header and verify context menus>>AddToContact", groups = { "smoke" })
+	public void AddToContactMessageHdrContextMenu() throws HarnessException {
+
+		// Create the message data to be sent
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String contactFirst = "First" + ZimbraSeleniumProperties.getUniqueString();
+
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+						"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<e t='c' a='"+ ZimbraAccount.AccountB().EmailAddress +"'/>" +
+						"<su>"+ subject +"</su>" +
+						"<mp ct='text/plain'>" +
+						"<content>"+ "body" + ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+						"</m>" +
+				"</SendMsgRequest>");
+
+		// Get all the SOAP data for later verification
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail actual = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);		
+		ZAssert.assertEquals(actual.zGetMailProperty(com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail.Field.From), ZimbraAccount.AccountA().EmailAddress, "Verify the From matches");
+
+		//	String OriginalEmailAddr = app.zPageMail.sGetText(Locators.zToAddressBubble);
+		app.zPageMail.zRightClickAddressBubble(Field.From);
+		app.zPageMail.AddToContactMsgHdrContextMenu();
+		SleepUtil.sleepMedium();
+
+		app.zPageMail.sFocus("css=input[id$='_FIRST_input']");
+		app.zPageMail.sType("css=input[id$='_FIRST_input']", contactFirst);
+		app.zPageMail.sClickAt(FormContactNew.Toolbar.SAVE, "");
+		SleepUtil.sleepMedium();
+
+		// -- Data Verification
+
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='contact'>"
+						+ "<query>#firstname:" + contactFirst + "</query>"
+						+ "</SearchRequest>");
+		String contactId = app.zGetActiveAccount().soapSelectValue("//mail:cn",
+				"id");
+
+		ZAssert.assertNotNull(contactId,"Verify the contact is returned in the search");
+
+		app.zGetActiveAccount().soapSend(
+				"<GetContactsRequest xmlns='urn:zimbraMail'>" + "<cn id='"
+						+ contactId + "'/>" + "</GetContactsRequest>");
+
+		String firstname = app.zGetActiveAccount().soapSelectValue(
+				"//mail:cn[@id='" + contactId + "']//mail:a[@n='firstName']",
+				null);
+		String email = app.zGetActiveAccount().soapSelectValue(
+				"//mail:cn[@id='" + contactId + "']//mail:a[@n='email']", null);
+
+		ZAssert.assertEquals(firstname, contactFirst,"Verify the first name was saved correctly");
+		ZAssert.assertEquals(email, ZimbraAccount.AccountA().EmailAddress,"Verify the email was saved correctly");
+
+	}
+
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/contextmenu/NewEmailMsgHdrContextMenu.java
@@ -0,0 +1,60 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose.contextmenu;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.PageMail.Locators;
+
+public class NewEmailMsgHdrContextMenu extends PrefGroupMailByMessageTest {
+
+	public NewEmailMsgHdrContextMenu() {
+		logger.info("New "
+				+ NewEmailMsgHdrContextMenu.class.getCanonicalName());
+
+		super.startingAccountPreferences.put("zimbraPrefComposeFormat", "text");
+
+	}
+
+	@Test(description = "Receive a  mail - Right Click From Msg Header and verify context menus>>New Emails", groups = { "smoke" })
+	public void NewEmailMessageHdrContextMenu() throws HarnessException {
+
+		// Create the message data to be sent
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+						"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<e t='c' a='"+ ZimbraAccount.AccountB().EmailAddress +"'/>" +
+						"<su>"+ subject +"</su>" +
+						"<mp ct='text/plain'>" +
+						"<content>"+ "body" + ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+						"</m>" +
+				"</SendMsgRequest>");
+
+		// Get all the SOAP data for later verification
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail actual = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);		
+		ZAssert.assertEquals(actual.zGetMailProperty(com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail.Field.From), ZimbraAccount.AccountA().EmailAddress, "Verify the From matches");
+
+		app.zPageMail.zRightClickAddressBubble(Field.From);
+		app.zPageMail.NewEmailMsgHdrContextMenu();
+		SleepUtil.sleepMedium();
+		logger.info(ZimbraAccount.AccountA().EmailAddress );
+		ZAssert.assertEquals(app.zPageMail.sGetText(Locators.zToAddressBubble), ZimbraAccount.AccountA().EmailAddress , "Compose window opens with email address should present in To field");	
+
+
+	}
+
+}
