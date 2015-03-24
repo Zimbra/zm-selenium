8ec463b305d55ebc54709330ac3eef8800a70d31
Nidhi Vyas <nidhi@zimbra.com>
2015-03-24 03:46:44 -0500

    (split)bug:97593, 49881
    Added test-cases to verify reset status of meetings after user has accepted the meetings and organizer makes following changes:
    1. Add Location
    2. Update Location
    3. Remove Location
    4. Add equipment
    5. Update Equipment
    6. Remove Equipment
    7. Modify Calendar.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97593
    https://bugzilla.zimbra.com/show_bug.cgi?id=49881
    
    Copied from Perforce
     Change: 540248

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/resetmeetingstatus/ResetStatusAfterModifyingCalendar.java
@@ -0,0 +1,154 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.organizer.singleday.resetmeetingstatus;
+
+import java.util.Calendar;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew.Field;
+
+public class ResetStatusAfterModifyingCalendar extends CalendarWorkWeekTest {	
+	
+	public ResetStatusAfterModifyingCalendar() {
+		logger.info("New "+ ResetStatusAfterModifyingCalendar.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+	
+	@Bugs(ids = "49881")
+	@Test(description = "Check reset status of meeting after modifying calendar",
+			groups = { "test1"
+					+ "	" })
+			
+	public void ResetStatusAfterModifyingCalendar_01() throws HarnessException {
+		
+		// Create a meeting			
+		String tz = ZTimeZone.TimeZoneEST.getID();
+		String apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		String apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		String apptCalendar = ZimbraSeleniumProperties.getUniqueString();
+		
+		// Create new calendar folder
+		FolderItem root = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.UserRoot);
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ apptCalendar +"' l='"+ root.getId() +"' view='appointment'/>" +
+                "</CreateFolderRequest>");
+		app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+		
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 6, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 7, 0, 0);
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" +
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        
+        //Login as attendee and accept the invite
+        app.zPageMain.zLogout();
+		app.zPageLogin.zLogin(ZimbraAccount.AccountA());
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+		app.zPageCalendar.zNavigateTo();
+		app.zPageCalendar.zListItem(Action.A_RIGHTCLICK, Button.O_ACCEPT_MENU, apptSubject);
+		
+		app.zPageMain.zLogout();			
+		
+		//Login as current account and modify
+		app.zPageLogin.zLogin(ZimbraAccount.AccountZWC());
+		
+        // Add attendee2 and re-send the appointment
+		app.zPageCalendar.zNavigateTo();
+        FormApptNew apptForm = (FormApptNew)app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);       
+        if(ZimbraSeleniumProperties.isWebDriver()){
+            String locator = "css=td[id$='_folderSelect'] td[id$='_select_container']";
+            apptForm.sClickAt(locator, "");            
+
+            locator = "//div[@id='z_shell']/div[contains(@id,'_Menu_') and contains(@class, 'DwtMenu')]";   
+            int count = apptForm.sGetXpathCount(locator);           
+            for  (int  i = 1; i <= count; i++) {
+        	String calPullDown = locator + "[position()=" + i + "]//tr//*[contains(text(),'" + apptCalendar + "')]";
+        	if(apptForm.zIsVisiblePerPosition(calPullDown, 0, 0)){
+        	    apptForm.sClickAt(calPullDown, "");
+        	    break;
+        	}        	
+            }            
+        }else{
+            apptForm.zFillField(Field.CalendarFolder, apptCalendar);
+        }
+        apptForm.zToolbarPressButton(Button.B_SEND);
+        SleepUtil.sleepVeryLong(); 
+        
+		// --- Check that the organizer shows the attendee as "Accepted" ---
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+			+		"<query>"+ apptSubject +"</query>"
+			+	"</SearchRequest>");
+	
+		String organizerInvId = app.zGetActiveAccount().soapSelectValue("//mail:appt", "invId");
+
+			app.zGetActiveAccount().soapSend(
+				"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ organizerInvId +"'/>");
+	
+		String attendeeStatus = app.zGetActiveAccount().soapSelectValue("//mail:at[@a='"+ ZimbraAccount.AccountA().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=AC
+		ZAssert.assertEquals(attendeeStatus, "AC", "Verify that the attendee shows as 'Accepted'");
+		
+		// --- Check that the attendee showing status as "Accepted" ---		
+		ZimbraAccount.AccountA().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+			+		"<query>"+ apptSubject +"</query>"
+			+	"</SearchRequest>");
+	
+		String attendeeInvId = ZimbraAccount.AccountA().soapSelectValue("//mail:appt", "invId");
+
+		ZimbraAccount.AccountA().soapSend(
+				"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ attendeeInvId +"'/>");
+	
+		String myStatus = ZimbraAccount.AccountA().soapSelectValue("//mail:at[@a='"+ ZimbraAccount.AccountA().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=AC
+		ZAssert.assertEquals(myStatus, "AC", "Verify that the attendee shows as 'Accepted'");
+		
+		
+        
+		
+	}
+	
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/resetmeetingstatus/ResetStatusAfterRemovingLocation.java
@@ -0,0 +1,129 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.organizer.singleday.resetmeetingstatus;
+
+import java.util.Calendar;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+
+public class ResetStatusAfterRemovingLocation extends CalendarWorkWeekTest {	
+	
+	public ResetStatusAfterRemovingLocation() {
+		logger.info("New "+ ResetStatusAfterRemovingLocation.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+	
+	@Bugs(ids = "49881")
+	@Test(description = "Check reset status of meeting after removing attendee",
+			groups = { "functional" })
+			
+	public void ResetStatusAfterRemovingLocation_01() throws HarnessException {
+		
+		// Create a meeting			
+		String tz = ZTimeZone.TimeZoneEST.getID();
+		String apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		String apptAttendee = ZimbraAccount.AccountA().EmailAddress;
+		ZimbraResource location = new ZimbraResource(ZimbraResource.Type.LOCATION);
+		String apptLocation = location.EmailAddress;
+		
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 13, 0, 0);
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"' loc='"+ apptLocation + "'>" +
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" + 
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee + "' d='2'/>" +
+                     		"<at cutype='RES' a='" + apptLocation + "' rsvp='1' role='REQ' url='" + apptLocation + "' ptst='AC'/>" +
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<e a='"+ apptLocation +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        
+        //Login as attendee and accept the invite
+        app.zPageMain.zLogout();
+		app.zPageLogin.zLogin(ZimbraAccount.AccountA());
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+		app.zPageCalendar.zNavigateTo();
+		app.zPageCalendar.zListItem(Action.A_RIGHTCLICK, Button.O_ACCEPT_MENU, apptSubject);		
+		app.zPageMain.zLogout();			
+		
+		//Login as current account and modify appointment
+		app.zPageLogin.zLogin(ZimbraAccount.AccountZWC());		
+		app.zPageCalendar.zNavigateTo();
+        
+        // Remove location and re-send the appointment
+        FormApptNew apptForm = (FormApptNew)app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+        apptForm.zRemoveLocation(apptLocation);
+        apptForm.zToolbarPressButton(Button.B_SEND);
+        SleepUtil.sleepVeryLong(); 
+        
+		// --- Check that the organizer shows the attendee as "Needs Action" ---
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+			+		"<query>"+ apptSubject +"</query>"
+			+	"</SearchRequest>");
+	
+		String organizerInvId = app.zGetActiveAccount().soapSelectValue("//mail:appt", "invId");
+
+			app.zGetActiveAccount().soapSend(
+				"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ organizerInvId +"'/>");
+	
+		String attendeeStatus = app.zGetActiveAccount().soapSelectValue("//mail:at[@a='"+ ZimbraAccount.AccountA().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=NE
+		ZAssert.assertEquals(attendeeStatus, "NE", "Verify that the attendee shows as 'Needs Action'");
+		
+		// --- Check that the attendee showing status as "Needs Action" ---
+		ZimbraAccount.AccountA().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
+			+		"<query>"+ apptSubject +"</query>"
+			+	"</SearchRequest>");
+	
+		String attendeeInvId = ZimbraAccount.AccountA().soapSelectValue("//mail:appt", "invId");
+
+		ZimbraAccount.AccountA().soapSend(
+				"<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ attendeeInvId +"'/>");
+	
+		String myStatus = ZimbraAccount.AccountA().soapSelectValue("//mail:at[@a='"+ ZimbraAccount.AccountA().EmailAddress +"']", "ptst");
+
+		// Verify attendee status shows as ptst=NE
+		ZAssert.assertEquals(myStatus, "NE", "Verify that the attendee shows as 'Needs Action'");
+		
+		
+        
+		
+	}
+	
+}
