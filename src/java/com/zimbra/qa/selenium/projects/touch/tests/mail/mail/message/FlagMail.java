ffba0150e8b1d6b86fe989ba4742a60d70484f68
Ankit Kumar <kumara@zimbra.com>
2015-04-01 02:29:22 -0500

    (split)bug:97751,97752,97753
    Added :Flag mail,unflag mail,mark read mai & mark unread mail test scripts
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97751
    https://bugzilla.zimbra.com/show_bug.cgi?id=97752
    https://bugzilla.zimbra.com/show_bug.cgi?id=97753
    
    Copied from Perforce
     Change: 540957

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/mail/mail/message/FlagMail.java
@@ -0,0 +1,89 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.mail.mail.message;
+
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.items.TagItem;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.PrefGroupMailByMessageTest;
+
+public class FlagMail extends PrefGroupMailByMessageTest {
+
+	public FlagMail() {
+		logger.info("New "+ FlagMail.class.getCanonicalName());
+	}
+	
+	@Test( description = "mark mail flag",
+			groups = { "smoke" })
+			
+	public void FlagMail_01() throws HarnessException {
+		
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String body = "text <strong>bold"+ ZimbraSeleniumProperties.getUniqueString() +"</strong> text";
+		String htmlBody = XmlStringUtil.escapeXml(
+				"<html>" +
+					"<head></head>" +
+					"<body>"+ body +"</body>" +
+				"</html>");
+
+		// Create the message data to be sent
+				ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+					"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<e t='c' a='"+ ZimbraAccount.AccountB().EmailAddress +"'/>" +
+						"<su>"+ subject +"</su>" +
+						"<mp ct='multipart/alternative'>" +
+						"<mp ct='text/plain'>" +
+							"<content>"+ body +"</content>" +
+						"</mp>" +
+						"<mp ct='text/html'>" +
+							"<content>"+ htmlBody +"</content>" +
+						"</mp>" +
+					"</mp>" +
+					"</m>" +
+				"</SendMsgRequest>");
+				app.zPageMail.zRefresh();
+				// Create a mail item to represent the message
+				MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+				//ZAssert.assertStringDoesNotContain(mail.getFlags(), "u", "Verify message is initially unread");
+	
+		
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, Button.B_FLAG_CONVERSATION, subject);
+		
+
+	
+		// SOAP verification
+				mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+				ZAssert.assertStringDoesNotContain(mail.getFlags(), "u", "Verify the message is flagged in the server");
+
+		
+		//UI VERIFICATION TO DO
+				// Make sure the GUI shows "flagged"
+				
+		
+			//	ZAssert.assertNotNull(listmail, "Verify the message is in the list");
+				//ZAssert.assertTrue(listmail.gIsFlagged, "Verify the message is flagged in the list");
+	}
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/mail/mail/message/MarkReadMail.java
@@ -0,0 +1,85 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.mail.mail.message;
+
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.items.TagItem;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.PrefGroupMailByMessageTest;
+
+public class MarkReadMail extends PrefGroupMailByMessageTest {
+
+	public MarkReadMail() {
+		logger.info("New "+ MarkReadMail.class.getCanonicalName());
+	}
+	
+	@Test( description = "mark mail as read",
+			groups = { "smoke" })
+			
+	public void readMail_01() throws HarnessException {
+		
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String body = "text <strong>bold"+ ZimbraSeleniumProperties.getUniqueString() +"</strong> text";
+		String htmlBody = XmlStringUtil.escapeXml(
+				"<html>" +
+					"<head></head>" +
+					"<body>"+ body +"</body>" +
+				"</html>");
+
+		// Create the message data to be sent
+				ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+					"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<e t='c' a='"+ ZimbraAccount.AccountB().EmailAddress +"'/>" +
+						"<su>"+ subject +"</su>" +
+						"<mp ct='multipart/alternative'>" +
+						"<mp ct='text/plain'>" +
+							"<content>"+ body +"</content>" +
+						"</mp>" +
+						"<mp ct='text/html'>" +
+							"<content>"+ htmlBody +"</content>" +
+						"</mp>" +
+					"</mp>" +
+					"</m>" +
+				"</SendMsgRequest>");
+				app.zPageMail.zRefresh();
+				// Create a mail item to represent the message
+				MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+				//ZAssert.assertStringDoesNotContain(mail.getFlags(), "u", "Verify message is initially unread");
+	
+		
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, Button.B_MARK_CONVERSATION_UNREAD, subject);
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, Button.B_MARK_CONVERSATION_READ, subject);
+		
+	
+		// SOAP verification
+				mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+				
+		// UI VERIFICATION
+				ZAssert.assertStringDoesNotContain(mail.getFlags(), "u", "Verify the message is marked read in the server");
+
+		
+		
+		
+	}
+}
