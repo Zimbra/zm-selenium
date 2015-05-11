3108323bcd8b1e83d8bef29b8e899c3b1d064f12
Nidhi Vyas <nidhi@zimbra.com>
2015-05-11 06:15:08 -0500

    (split)bug:65749,97593 Added test case for Reply to Cancelled Appointments not using zimbraPrefFromAddress
    https://bugzilla.zimbra.com/show_bug.cgi?id=65749
    https://bugzilla.zimbra.com/show_bug.cgi?id=97593
    
    Copied from Perforce
     Change: 544270

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/actions/Cancel.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/actions/Cancel.java
@@ -17,7 +17,9 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.organizer.singleday.actions;
 
 import java.util.Calendar;
+
 import org.testng.annotations.*;
+
 import com.zimbra.qa.selenium.framework.core.Bugs;
 import com.zimbra.qa.selenium.framework.items.*;
 import com.zimbra.qa.selenium.framework.ui.*;
@@ -97,6 +99,81 @@ public class Cancel extends CalendarWorkWeekTest {
 		//		AppointmentItem canceledAppt = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")", startUTC, endUTC);
 		AppointmentItem canceledAppt = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")");
 		ZAssert.assertNull(canceledAppt, "Verify meeting is deleted from attendee's calendar");
+	}
+
+	
+	@Bugs(ids = "65749")
+	@Test(description = "Reply to Cancelled Appointments not using zimbraPrefFromAddress",
+			groups = { "test" })
+	public void CancelMeeting_02() throws HarnessException {
+		
+		String tz, apptSubject, apptBody, apptAttendee1, fromAddress;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		apptBody = ZimbraSeleniumProperties.getUniqueString();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		fromAddress = ZimbraSeleniumProperties.getUniqueString() + "." + ZimbraSeleniumProperties.getUniqueString() + "@" + ZimbraSeleniumProperties.getStringProperty("testdomain", "testdomain.com");
+		
+	    ZimbraAdminAccount.GlobalAdmin().soapSend(
+	    	      "<ModifyAccountRequest xmlns='urn:zimbraAdmin'><id>" + 
+	    	      app.zGetActiveAccount().ZimbraId + "</id>" + 
+	    	      "<a n='zimbraPrefFromAddress'>" + fromAddress + "</a>" + 
+	    	      "</ModifyAccountRequest>");
+		app.zPageLogin.zNavigateTo();
+		this.startingPage.zNavigateTo();
+	    
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     "<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     "<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     "<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     "<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     "<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" + 
+                     "</inv>" +
+                     "<e a='"+ apptAttendee1 +"' t='t'/>" +
+                     "<mp content-type='text/plain'>" +
+                     "<content>"+ apptBody +"</content>" +
+                     "</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+		
+        // Refresh the view
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        
+        // Select the appointment
+        app.zPageCalendar.zListItem(Action.A_LEFTCLICK, apptSubject);
+        
+        // Right Click -> Delete context menu
+        DialogWarning dialog = (DialogWarning)app.zPageCalendar.zListItem(Action.A_RIGHTCLICK, Button.O_CANCEL_MENU, apptSubject);
+
+        // Click Send Cancellation
+		dialog.zClickButton(Button.B_SEND_CANCELLATION);
+		
+		// Verify the meeting disappears from the view
+		ZAssert.assertEquals(app.zPageCalendar.zIsAppointmentExists(apptSubject), false, "Verify meeting is deleted from organizer's calendar");
+		
+		// Verify meeting is deleted from attendee's calendar
+		ZimbraAccount.AccountA().soapSend(
+				"<SearchRequest types='message' xmlns='urn:zimbraMail'>"
+		+			"<query>in:inbox subject:(Cancelled) subject:("+ apptSubject +")</query>"
+		+		"</SearchRequest>");
+	String id = ZimbraAccount.AccountA().soapSelectValue("//mail:m", "id");
+
+	ZimbraAccount.AccountA().soapSend(
+				"<GetMsgRequest xmlns='urn:zimbraMail'>"
+		+			"<m id='"+ id +"' html='1'/>"
+		+		"</GetMsgRequest>");
+
+	// Verify From: alias
+	String address = ZimbraAccount.AccountA().soapSelectValue("//mail:e[@t='f']", "a");
+	ZAssert.assertEquals(address, fromAddress, "Verify the from is the alias email address");
 	}
 
 }
