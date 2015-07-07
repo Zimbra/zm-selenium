f923067ee1bffb2c550622b878c9fbbe5e317351
Ankit Kumar <kumara@zimbra.com>
2015-07-07 02:05:12 -0500

    (split)bug:100291
    - added new test script of moving mount point.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100291
    
    Copied from Perforce
     Change: 549705

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/mail/mountpoints/MoveMount.java
@@ -0,0 +1,93 @@
+package com.zimbra.qa.selenium.projects.touch.tests.mail.mountpoints;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.touch.ui.PageCreateFolder;
+
+public class MoveMount extends PrefGroupMailByMessageTest {
+
+
+
+	public MoveMount() {
+		logger.info("New "+ MoveMount.class.getCanonicalName());
+
+	}
+
+	@Test(	description = "Move a mountpoint to a folder",
+			groups = { "functional" })
+	
+	public void MoveMountpoint_01() throws HarnessException {
+		FolderItem userRoot = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.UserRoot);
+		String folderName = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		ZimbraAccount Owner = (new ZimbraAccount()).provision().authenticate();
+
+		// Owner creates a folder, shares it with current user
+		String ownerFoldername = "ownerfolder"+ ZimbraSeleniumProperties.getUniqueString();
+
+		FolderItem ownerInbox = FolderItem.importFromSOAP(Owner, FolderItem.SystemFolder.Inbox);
+		ZAssert.assertNotNull(ownerInbox, "Verify the new owner folder exists");
+
+		Owner.soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+						+		"<folder name='" + ownerFoldername +"' l='" + ownerInbox.getId() +"'/>"
+						+	"</CreateFolderRequest>");
+
+		FolderItem ownerFolder = FolderItem.importFromSOAP(Owner, ownerFoldername);
+		ZAssert.assertNotNull(ownerFolder, "Verify the new owner folder exists");
+
+		Owner.soapSend(
+				"<FolderActionRequest xmlns='urn:zimbraMail'>"
+						+		"<action id='"+ ownerFolder.getId() +"' op='grant'>"
+						+			"<grant d='" + app.zGetActiveAccount().EmailAddress + "' gt='usr' perm='r'/>"
+						+		"</action>"
+						+	"</FolderActionRequest>");
+
+
+		// Current user creates the mountpoint that points to the share
+		String mountpointname = "mountpoint"+ ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+				"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+						+		"<link l='1' name='"+ mountpointname +"' view='message' rid='"+ ownerFolder.getId() +"' zid='"+ Owner.ZimbraId +"'/>"
+						+	"</CreateMountpointRequest>");
+
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+		ZAssert.assertNotNull(mountpoint, "Verify the subfolder is available");
+
+		// Create a folder
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+						"<folder name='"+ folderName +"' l='"+ userRoot.getId() +"'/>" +
+				"</CreateFolderRequest>");
+		FolderItem folder = FolderItem.importFromSOAP(app.zGetActiveAccount(), folderName);
+		ZAssert.assertNotNull(folderName, "Verify the subfolder is available");
+		app.zPageMain.zRefresh();	
+
+		// Select the mount folder from the list
+		PageCreateFolder createFolderPage = new PageCreateFolder(app, startingPage);
+		createFolderPage.zClickButton(Button.B_EDIT);
+		createFolderPage.zSelectFolder(mountpointname);
+
+		// Moving folder to another folder
+		createFolderPage.zClickButton(Button.B_LOCATION);
+		createFolderPage.zSelectFolder(folderName);
+		createFolderPage.zClickButton(Button.B_SAVE);
+
+		// Click Get Mail button
+		app.zPageMain.zRefresh();	
+
+		// SOAP Verify the folder is in the other Subfolder
+		mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+		ZAssert.assertNotNull(mountpoint, "Verify the mountpoint is again available");
+		ZAssert.assertEquals(folder.getId(), mountpoint.getParentId(), "Verify the mountpoint's parent is now the other folder");
+
+		//To do- UI is not refreshing in automation
+		// UI Verify the folder is in the other Subfolder
+
+	}
+
+}
