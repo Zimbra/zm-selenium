4941c4a0afc47b92ce96352cd46f52aeae5fb806
Ankit Kumar <kumara@zimbra.com>
2015-07-09 08:02:16 -0500

    (split)bug:100291
    -Added mark unread mail in mountpoint with view rights
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100291
    
    Copied from Perforce
     Change: 550091

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/mail/mountpoints/viewer/MarkUnReadMail.java
@@ -0,0 +1,99 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.touch.tests.mail.mountpoints.viewer;
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.touch.ui.PageCreateFolder;
+
+public class MarkUnReadMail extends PrefGroupMailByMessageTest{
+
+
+	public MarkUnReadMail() {
+		logger.info("New " + MarkUnReadMail.class.getCanonicalName());
+	}
+
+	@Test( description = "Verify error message is displayed when on deleting email on mountpoint message (read-only share)",
+			groups = { "functional" })
+
+	public void MarkReadMail_01() throws HarnessException  {
+
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String mountpointname = "mountpoint" + ZimbraSeleniumProperties.getUniqueString();
+
+		FolderItem inbox = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox);
+
+		// Create a folder to share
+		ZimbraAccount.AccountA().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+						+		"<folder name='" + foldername + "' l='" + inbox.getId() + "'/>"
+						+	"</CreateFolderRequest>");
+
+		FolderItem folder = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), foldername);
+
+		// Share it
+		ZimbraAccount.AccountA().soapSend(
+				"<FolderActionRequest xmlns='urn:zimbraMail'>"
+						+		"<action id='"+ folder.getId() +"' op='grant'>"
+						+			"<grant d='"+ app.zGetActiveAccount().EmailAddress +"' gt='usr' perm='r'/>"
+						+		"</action>"
+						+	"</FolderActionRequest>");
+
+		// Add a message to it
+		ZimbraAccount.AccountA().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+						+		"<m l='"+ folder.getId() +"' >"
+						+			"<content>From: foo@foo.com\n"
+						+				"To: foo@foo.com \n"
+						+				"Subject: "+ subject +"\n"
+						+				"MIME-Version: 1.0 \n"
+						+				"Content-Type: text/plain; charset=utf-8 \n"
+						+				"Content-Transfer-Encoding: 7bit\n"
+						+				"\n"
+						+				"simple text string in the body\n"
+						+			"</content>"
+						+		"</m>"
+						+	"</AddMsgRequest>");
+
+
+		// Mount it
+		app.zGetActiveAccount().soapSend(
+				"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+						+		"<link l='1' name='"+ mountpointname +"'  rid='"+ folder.getId() +"' zid='"+ ZimbraAccount.AccountA().ZimbraId +"'/>"
+						+	"</CreateMountpointRequest>");
+
+		app.zPageMain.zRefresh();	
+
+		//Mark mail as unread then read
+		PageCreateFolder createFolderPage = new PageCreateFolder(app, startingPage);
+		createFolderPage.zSelectMountFolder(mountpointname);
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, Button.B_MARK_CONVERSATION_UNREAD, subject);
+
+		//UI Verification - Verify error message is displayed 
+		app.zPageMail.zVerifyErrorMessageExist();
+
+		// SOAP verification
+
+		//UI Verification - Verify error message is displayed 
+		app.zPageMail.zVerifyErrorMessageExist();
+
+	}
+}
