d2fa71b0eb1a8c6b7c69eb492c36e4a6d8c78ac8
Pallavi Khairnar <pkhairnar@zimbra.com>
2015-07-20 03:45:44 -0500

    (split)bug: 83959, 99262 Added test cases to verify search contacts functionality
    https://bugzilla.zimbra.com/show_bug.cgi?id=83959
    https://bugzilla.zimbra.com/show_bug.cgi?id=99262
    
    Copied from Perforce
     Change: 550848

---
+++ src/java/com/zimbra/qa/selenium/projects/touch/tests/contacts/contacts/SearchContact.java
@@ -0,0 +1,143 @@
+/*
+
+ * ***** BEGIN LICENSE BLOCK *****
+
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+
+ */
+
+package com.zimbra.qa.selenium.projects.touch.tests.contacts.contacts;
+
+
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.ContactItem;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.touch.core.TouchCommonTest;
+
+public class SearchContact extends TouchCommonTest  {
+	
+	public SearchContact() {
+		logger.info("New "+ CreateContact.class.getCanonicalName());
+		super.startingPage = app.zPageAddressbook;
+	}
+
+	@Test(description = "Search contact item with first name",
+			groups = { "smoke" })
+
+	public void CreateContact_01() throws HarnessException{
+
+		//-- DATA
+		
+		// Create a contact item
+		ContactItem contact = new ContactItem();
+		contact.firstName = "First" + ZimbraSeleniumProperties.getUniqueString();
+		contact.lastName = "Last" + ZimbraSeleniumProperties.getUniqueString();
+		contact.email = "email" + ZimbraSeleniumProperties.getUniqueString() + "@domain.com";
+		String locator = contact.lastName + ", " + contact.firstName;
+
+		app.zGetActiveAccount().soapSend(
+	                "<CreateContactRequest xmlns='urn:zimbraMail'>" +
+	                		"<cn >" +
+	                			"<a n='firstName'>" + contact.firstName +"</a>" +
+	                			"<a n='lastName'>" + contact.lastName +"</a>" +
+	                			"<a n='email'>" + contact.email + "</a>" +
+                			"</cn>" +
+	                "</CreateContactRequest>");
+
+		//-- Data Verification
+		
+		// Refresh to get the contact into the client
+		app.zPageAddressbook.zRefresh();
+		
+		// Enter first name & search contact
+		app.zPageAddressbook.zSearchContact(Button.B_SEARCH, contact.firstName);
+		
+		//GUI verification    
+		ZAssert.assertEquals(app.zPageAddressbook.zVerifyContactExists(locator), true, "Verify contact is removed from Trash folder");
+		
+	}
+
+	@Test(description = "Search contact item with last name",
+			groups = { "functional" })
+
+	public void CreateContact_02() throws HarnessException {
+
+		//-- DATA
+		
+		// Create a contact item
+		ContactItem contact = new ContactItem();
+		contact.firstName = "First" + ZimbraSeleniumProperties.getUniqueString();
+		contact.lastName = "Last" + ZimbraSeleniumProperties.getUniqueString();
+		contact.email = "email" + ZimbraSeleniumProperties.getUniqueString() + "@domain.com";
+		String locator = contact.lastName + ", " + contact.firstName;
+
+		app.zGetActiveAccount().soapSend(
+	                "<CreateContactRequest xmlns='urn:zimbraMail'>" +
+	                		"<cn >" +
+	                			"<a n='firstName'>" + contact.firstName +"</a>" +
+	                			"<a n='lastName'>" + contact.lastName +"</a>" +
+	                			"<a n='email'>" + contact.email + "</a>" +
+                			"</cn>" +
+	                "</CreateContactRequest>");
+
+		//-- Data Verification
+		
+		// Refresh to get the contact into the client
+		app.zPageAddressbook.zRefresh();
+		
+		// Enter last name & search contact
+		app.zPageAddressbook.zSearchContact(Button.B_SEARCH, contact.lastName);
+		
+		//GUI verification    
+		ZAssert.assertEquals(app.zPageAddressbook.zVerifyContactExists(locator), true, "Verify contact is removed from Trash folder");
+		
+	}
+	
+	@Test(description = "Search contact item with email",
+			groups = { "functional" })
+
+	public void CreateContact_03() throws HarnessException {
+
+		//-- DATA
+		
+		// Create a contact item
+		ContactItem contact = new ContactItem();
+		contact.firstName = "First" + ZimbraSeleniumProperties.getUniqueString();
+		contact.lastName = "Last" + ZimbraSeleniumProperties.getUniqueString();
+		contact.email = "email" + ZimbraSeleniumProperties.getUniqueString() + "@domain.com";
+		String locator = contact.lastName + ", " + contact.firstName;
+
+		app.zGetActiveAccount().soapSend(
+	                "<CreateContactRequest xmlns='urn:zimbraMail'>" +
+	                		"<cn >" +
+	                			"<a n='firstName'>" + contact.firstName +"</a>" +
+	                			"<a n='lastName'>" + contact.lastName +"</a>" +
+	                			"<a n='email'>" + contact.email + "</a>" +
+                			"</cn>" +
+	                "</CreateContactRequest>");
+		
+		// Refresh to get the contact into the client
+		app.zPageAddressbook.zRefresh();
+		
+		// Search contact with email
+		app.zPageAddressbook.zSearchContact(Button.B_SEARCH, contact.email);
+		
+		//GUI verification    
+		ZAssert.assertEquals(app.zPageAddressbook.zVerifyContactExists(locator), true, "Verify contact is removed from Trash folder");
+		
+	}
+
+}
