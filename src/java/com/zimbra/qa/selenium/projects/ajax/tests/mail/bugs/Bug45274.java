799b69be2e23ca4b1c918eb1a2010b7b8d77a780
Pallavi Khairnar <pkhairnar@zimbra.com>
2015-07-24 02:30:52 -0500

    (split)Bug: 45274, 99262 Added test case to verify that text signature is getting added while composing mail in HTML
    https://bugzilla.zimbra.com/show_bug.cgi?id=45274
    https://bugzilla.zimbra.com/show_bug.cgi?id=99262
    
    Copied from Perforce
     Change: 551234

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/bugs/Bug45274.java
@@ -0,0 +1,121 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.bugs;
+
+import java.util.HashMap;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.items.RecipientItem;
+import com.zimbra.qa.selenium.framework.items.SignatureItem;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.AppAjaxClient;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew;
+
+public class Bug45274 extends AjaxCommonTest {
+	String sigName = "signame" + ZimbraSeleniumProperties.getUniqueString();
+	String sigBody = "sigbody" + ZimbraSeleniumProperties.getUniqueString();
+
+	@SuppressWarnings("serial")
+	public Bug45274() {
+		super.startingPage = app.zPageMail;
+		super.startingAccountPreferences = new HashMap<String, String>() {
+			{
+				put("zimbraPrefComposeFormat", "html");
+			}
+		};
+	}
+
+	@BeforeMethod(groups = { "always" })
+	public void CreateSignature() throws HarnessException {
+		ZimbraAccount.AccountZWC().authenticate();
+		ZimbraAccount.AccountZWC().soapSend(
+				"<CreateSignatureRequest xmlns='urn:zimbraAccount'>"
+				+ "<signature name='" + this.sigName + "' >"
+				+ "<content type='text/plain'>" + this.sigBody
+				+ "</content>" + "</signature>"
+				+ "</CreateSignatureRequest>");
+
+		// Logout and login
+		this.app.zPageLogin.zNavigateTo();
+		this.app.zPageMail.zNavigateTo();
+
+		logger.info("CreateSignature: finish");
+
+	}
+
+	/**
+	 * Test case : Create signature through soap Compose HTML message and add
+	 * text signature Send mail to self and verify signature through soap.
+	 * 
+	 * @throws HarnessException
+	 */
+	@Bugs(ids="45274")
+	@Test(description = " Compose html Msg with text signature and Verify signature thropugh soap", groups = { "functional" })
+	public void Bug45274_01() throws HarnessException {
+		
+		
+		// Signature is created
+		SignatureItem signature = SignatureItem.importFromSOAP(app.zGetActiveAccount(), this.sigName);
+		ZAssert.assertEquals(signature.getName(), this.sigName,"verified Text Signature is created");
+		
+		// Create the message data to be sent
+		MailItem mail = new MailItem();
+		mail.dToRecipients.add(new RecipientItem(ZimbraAccount.AccountZWC()));
+		mail.dSubject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		mail.dBodyText = "body" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Open the new mail form
+		FormMailNew mailform = (FormMailNew) app.zPageMail.zToolbarPressButton(Button.B_NEW);
+		ZAssert.assertNotNull(mailform, "Verify the new form opened");
+
+		// Fill out the form with the data
+		mailform.zFill(mail);
+		 //click Signature drop down and add signature
+		app.zPageMail.zToolbarPressPulldown(Button.B_OPTIONS,Button.O_ADD_SIGNATURE,this.sigName);
+		
+		DialogWarning dialog = new DialogWarning(DialogWarning.DialogWarningID.ComposeOptionsChangeWarning,app,((AppAjaxClient) app).zPageMail);
+		if(dialog.zIsActive()){
+		//ZAssert.assertTrue(dialog.zIsActive(), "Verify the warning dialog opens");
+		dialog.zClickButton(Button.B_OK);
+		}
+	
+		// Send the message
+		mailform.zSubmit();
+
+		MailItem received = MailItem.importFromSOAP(ZimbraAccount.AccountZWC(),"in:inbox subject:(" + mail.dSubject + ")");
+
+		logger.debug("===========received is: " + received);
+		logger.debug("===========app is: " + app);
+		
+		//Verify TO, Subject, Body,Signature
+
+		ZAssert.assertEquals(received.dFromRecipient.dEmailAddress, app.zGetActiveAccount().EmailAddress,"Verify the from field is correct");
+		ZAssert.assertEquals(received.dToRecipients.get(0).dEmailAddress,ZimbraAccount.AccountZWC().EmailAddress,"Verify the to field is correct");
+		ZAssert.assertEquals(received.dSubject, mail.dSubject,"Verify the subject field is correct");
+		ZAssert.assertStringContains(received.dBodyText, mail.dBodyText,"Verify the body content is correct");
+		ZAssert.assertStringContains(received.dBodyText, this.sigBody,"Verify the signature is correct");
+
+	}
+}
