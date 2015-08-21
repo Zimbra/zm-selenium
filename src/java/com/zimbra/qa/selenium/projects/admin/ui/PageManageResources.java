b9f005ed37a43d311388c53071fa02ddd4c833cf
Jitesh Sojitra <jsojitra@zimbra.com>
2015-08-21 12:56:28 -0500

    (split)bug:100851 - Fixing issue of navigate way with sClick selenium method
    https://bugzilla.zimbra.com/show_bug.cgi?id=100851
    
    Copied from Perforce
     Change: 553493

--- src/java/com/zimbra/qa/selenium/projects/admin/ui/PageManageResources.java
+++ src/java/com/zimbra/qa/selenium/projects/admin/ui/PageManageResources.java
@@ -339,7 +339,7 @@ public class PageManageResources extends AbsTab {
 					throw new HarnessException("Button " + pulldown + " option " + option + " optionLocator " + optionLocator + " not present!");
 				}
 
-				this.sClickAt(optionLocator,"");
+				this.zClickAt(optionLocator,"");
 
 				// If the app is busy, wait for it to become active
 				//zWaitForBusyOverlay();
