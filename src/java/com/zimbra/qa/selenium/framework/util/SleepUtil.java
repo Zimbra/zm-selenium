e322d36438619b1175ed48b301aca8b50b728525
Girish Bhamare <girish@zimbra.com>
2015-10-26 01:05:19 -0500

    (split)bug:100773
    -- With the coverage enable, it requires more sleep due to slowness, so added following condtion
    if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100773
    
    Copied from Perforce
     Change: 557973

--- src/java/com/zimbra/qa/selenium/framework/util/SleepUtil.java
+++ src/java/com/zimbra/qa/selenium/framework/util/SleepUtil.java
@@ -37,20 +37,20 @@ public class SleepUtil {
 
 	/// Public methods
 	public static int SleepGranularity = 1000;
-	
+
 	public static void sleep(long millis) {
-		
+
 		Date start = new Date();
-		
+
 		try {
-		
+
 			long target = millis;
 			long total = 0; // The total milliseconds slept in this method
-			
+
 			try {
-				
+
 				while (millis > 0) {
-					
+
 					if ( millis >= SleepGranularity) {
 						logger.info("Sleep: "+ SleepGranularity +" milliseconds ... ("+ total +"/"+ target +")");
 						Thread.sleep(SleepGranularity);
@@ -60,53 +60,80 @@ public class SleepUtil {
 						Thread.sleep(millis);
 						total += millis;
 					}
-					
+
 					millis -= SleepGranularity;
 				}
-	
+
 			} catch (InterruptedException e) {
 				logger.warn("Sleep was interuppted", e);
 			}
-			
+
 		} finally {
 			SleepMetrics.RecordSleep((new Throwable()).getStackTrace(), millis, start, new Date());
 		}
 	}
-	
+
 	/**
 	 * Sleep a 500 msec
 	 */
 	public static void sleepVerySmall() {
+
+		if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")){
+
+			sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 1500));
+
+		}
 		sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 500));
 	}
-	
+
 	/**
 	 * Sleep a 1000 msec
 	 */
 	public static void sleepSmall() {
+
+		if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")){
+
+			sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 2500));
+
+		}
 		sleep(ZimbraSeleniumProperties.getIntProperty("small_wait", 1000));
 	}
-	
+
 	/**
 	 * Sleep a 2000 msec
 	 */
 	public static void sleepMedium() {
+		if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")){
+
+			sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 4500));
+
+		}
 		sleep(ZimbraSeleniumProperties.getIntProperty("medium_wait", 2000));
 	}
-	
+
 	/**
 	 * Sleep a 4000 msec
 	 */
 	public static void sleepLong() {
+		if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")){
+
+			sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 8000));
+
+		}
 		sleep(ZimbraSeleniumProperties.getIntProperty("long_wait", 4000));
 	}
-	
+
 	/**
 	 * Sleep a 10,000 msec
 	 */
 	public static void sleepVeryLong() {
+		if(ZimbraSeleniumProperties.getStringProperty("coverage.enabled").contains("true")){
+
+			sleep(ZimbraSeleniumProperties.getIntProperty("very_small_wait", 25000));
+
+		}
 		sleep(ZimbraSeleniumProperties.getIntProperty("very_long_wait", 10000));
 	}
-	
-	
+
+
 }
