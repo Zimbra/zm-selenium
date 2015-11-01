33eae366d89907cdc6dc8c2adeea02548f5abf8e
Jitesh Sojitra <jsojitra@zimbra.com>
2015-10-31 22:46:27 -0500

    (split)bug:100851 - Removed ViewMessage_02 because it already have harnessException and testcase didn't write ever properly
    https://bugzilla.zimbra.com/show_bug.cgi?id=100851
    
    Copied from Perforce
     Change: 558290

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/srchhighlighter/ViewMessage.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/srchhighlighter/ViewMessage.java
@@ -17,19 +17,14 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.zimlets.srchhighlighter;
 
 import java.util.*;
-
 import org.testng.annotations.*;
-
-import com.zimbra.qa.selenium.framework.core.Bugs;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.projects.ajax.core.*;
 import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail.Field;
 import com.zimbra.qa.selenium.projects.ajax.ui.search.*;
 
-
 public class ViewMessage extends AjaxCommonTest {
-
 	
 	public ViewMessage() {
 		logger.info("New "+ ViewMessage.class.getCanonicalName());
@@ -44,17 +39,14 @@ public class ViewMessage extends AjaxCommonTest {
 		    put("zimbraPrefGroupMailBy", "message");
 		}};
 
-
-
 	}
 	
 	@Test(	description = "Search for mail content.  Verify search terms are highlighted.",
 			groups = { "functional" })
+	
 	public void ViewMessage_01() throws HarnessException {
 		
-		
 		//-- DATA
-		
 		
 		// Create the message data to be sent
 		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
@@ -72,10 +64,7 @@ public class ViewMessage extends AjaxCommonTest {
 						"</m>" +
 					"</SendMsgRequest>");
 
-		
-		
 		//-- GUI
-		
 		
 		// Click Get Mail button
 		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
@@ -117,78 +106,5 @@ public class ViewMessage extends AjaxCommonTest {
 		}
 
 	}
-	@Bugs(ids="82607")
-	@Test(	description = "Search for mail content (two terms).  Verify search terms are highlighted.",
-			groups = { "functional" })
-	public void ViewMessage_02() throws HarnessException {
-		
-		
-		//-- DATA
-		
-		
-		// Create the message data to be sent
-		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
-		String term1 = "search" + ZimbraSeleniumProperties.getUniqueString();
-		String term2 = "search" + ZimbraSeleniumProperties.getUniqueString();
-		
-		// Send the message from AccountA to the ZWC user
-		ZimbraAccount.AccountA().soapSend(
-					"<SendMsgRequest xmlns='urn:zimbraMail'>" +
-						"<m>" +
-							"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
-							"<su>"+ subject +"</su>" +
-							"<mp ct='text/plain'>" +
-								"<content>"+ term1 +" "+ term2 +"</content>" +
-							"</mp>" +
-						"</m>" +
-					"</SendMsgRequest>");
-
-		
-		
-		//-- GUI
-		
-		
-		// Click Get Mail button
-		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
-
-		// Remember to close the search view
-		try {
-			
-			// Search for the message
-			app.zPageSearch.zAddSearchQuery("content:("+ term1 +" "+ term2 +")");
-			app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
-			
-			// Select the result
-			DisplayMail display = (DisplayMail)app.zPageSearch.zListItem(Action.A_LEFTCLICK, subject);
-			ZAssert.assertNotNull(display, "Verify the message contents are shown");
-
-			// Wait for a bit so the zimlet can take affect
-			SleepUtil.sleep(5000);
-
-			// Verify the term is highlighted
-			
-			@SuppressWarnings("unused")
-			HtmlElement body = display.zGetMailPropertyAsHtml(Field.Body);
-			
-			throw new HarnessException("See https://bugzilla.zimbra.com/show_bug.cgi?id=82607");
-			
-			// See http://bugzilla.zimbra.com/show_bug.cgi?id=82607 ... the DOM
-			// has a bug in it.  Once that bug is fixed, the xpath may need rework.
-			// 
-			// Verify the first term is located in a search highlight
-			
-		/*	Note:will comment out following statement once this big got fixed */
-			
-	      /*HtmlElement.evaluate(body, "//span/span[@class='ZmSearchResult']", null, term1, 1);
-			HtmlElement.evaluate(body, "//span/span[@class='ZmSearchResult']", null, term2, 1);*/
-
-		} finally {
-			// Remember to close the search view
-			app.zPageSearch.zClose();
-		}
-
-	}
-
-
 
 }
