129617c322330f8abc5aeb19084e1f213d99e917
Jitesh Sojitra <jsojitra@zimbra.com>
2016-02-23 11:14:16 -0600

    (split)bug:104082 - Integrating changes from JUDASPRIEST to main
    https://bugzilla.zimbra.com/show_bug.cgi?id=104082
    
    Copied from Perforce
     Change: 566488

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/email/HoverOverMessageBody.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/email/HoverOverMessageBody.java
@@ -19,6 +19,7 @@ package com.zimbra.qa.selenium.projects.ajax.tests.zimlets.email;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import com.zimbra.qa.selenium.framework.core.Bugs;
 import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
@@ -206,6 +207,198 @@ public class HoverOverMessageBody extends PrefGroupMailByMessageTest {
         	+		"</m>"
 			+	"</AddMsgRequest>");
 
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the message so that it shows in the reading pane
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Hover over the email address
+		String locator = "css=span[id$='_ZmEmailObjectHandler']:contains("+ email +")";
+		app.zPageMail.sMouseOver(locator, (WebElement[]) null);
+		
+		// Verify the contact tool tip opens
+		TooltipContact tooltip = new TooltipContact(app);
+		tooltip.zWaitForActive();
+		
+		ZAssert.assertTrue(tooltip.zIsActive(), "Verify the tooltip shows");
+		
+	}
+	
+	@Test(	description = "Hover over a contact in a message body (zimbraFeatureGalEnabled=FALSE)",
+			groups = { "functional" })
+	public void HoverOverMessageBody_05() throws HarnessException {
+
+		final String email = "email" + ZimbraSeleniumProperties.getUniqueString() + "@foo.com";
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateContactRequest xmlns='urn:zimbraMail'>" +
+						"<cn >" +
+							"<a n='firstName'>first"+ ZimbraSeleniumProperties.getUniqueString() +"</a>" +
+							"<a n='lastName'>last"+ ZimbraSeleniumProperties.getUniqueString() +"</a>" +
+							"<a n='email'>"+ email +"</a>" +
+						"</cn>" +
+				"</CreateContactRequest>" );
+
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='" + FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox).getId() + "' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"Line 1\n"
+        	+				"abc "+ email +" def\n"
+        	+				"Line 2\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the message so that it shows in the reading pane
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Hover over the email address
+		String locator = "css=span[id$='_ZmEmailObjectHandler']:contains("+ email +")";
+		app.zPageMail.sMouseOver(locator, (WebElement[]) null);
+		
+		// Verify the contact tool tip opens
+		TooltipContact tooltip = new TooltipContact(app);
+		tooltip.zWaitForActive();
+		
+		ZAssert.assertTrue(tooltip.zIsActive(), "Verify the tooltip shows");
+		
+		
+	}
+	
+	@Bugs(	ids = "80286" )
+	@Test(	description = "Hover over a GAL contact in a message body (zimbraFeatureGalEnabled=FALSE)",
+			groups = { "functional" })
+	public void HoverOverMessageBody_06() throws HarnessException {
+
+		// Create a contact in the GAL
+		ZimbraAccount contactGAL = (new ZimbraAccount()).provision().authenticate();
+		
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='" + FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox).getId() + "' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"Line 1\n"
+        	+				"abc "+ contactGAL.EmailAddress +" def\n"
+        	+				"Line 2\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the message so that it shows in the reading pane
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Hover over the email address
+		String locator = "css=span[id$='_ZmEmailObjectHandler']:contains("+ contactGAL.EmailAddress +")";
+		app.zPageMail.sMouseOver(locator, (WebElement[]) null);
+		
+		// Verify the contact tool tip opens
+		TooltipContact tooltip = new TooltipContact(app);
+		tooltip.zWaitForActive();
+		
+		ZAssert.assertTrue(tooltip.zIsActive(), "Verify the tooltip shows");
+		
+		
+	}
+
+	@Test(	description = "Hover over a contact group in a message body (zimbraFeatureGalEnabled=FALSE)",
+			groups = { "functional" })
+	public void HoverOverMessageBody_07() throws HarnessException {
+
+		String groupName = "group" + ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+			"<CreateContactRequest xmlns='urn:zimbraMail'>" +
+				"<cn >" +
+					"<a n='type'>group</a>" +
+					"<a n='nickname'>" + groupName +"</a>" +
+					"<a n='fileAs'>8:" +  groupName +"</a>" +
+			        "<m type='I' value='" + ZimbraAccount.AccountA().EmailAddress + "' />" +
+			        "<m type='I' value='" + ZimbraAccount.AccountB().EmailAddress + "' />" +
+				"</cn>" +
+			"</CreateContactRequest>");
+
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='" + FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox).getId() + "' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"Line 1\n"
+        	+				"abc "+ groupName +" def\n"
+        	+				"Line 2\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the message so that it shows in the reading pane
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Hover over the email address
+		String locator = "css=span[id$='_com_zimbra_email']:contains("+ groupName +")";
+		
+		// Verify the contact group is not converted to the email zimlet link
+		boolean present = app.zPageMail.sIsElementPresent(locator);
+		ZAssert.assertFalse(present, "Verify the contact group name is not highlighted");
+		
+		
+	}
+	
+	@Bugs(	ids = "80286" )
+	@Test(	description = "Hover over an unknown email address (zimbraFeatureGalEnabled=FALSE)",
+			groups = { "functional" })
+	public void HoverOverMessageBody_08() throws HarnessException {
+
+		final String email = "email" + ZimbraSeleniumProperties.getUniqueString() + "@foo.com";
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='" + FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox).getId() + "' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"Line 1\n"
+        	+				"abc "+ email +" def\n"
+        	+				"Line 2\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+		
 		// Refresh current view
 		app.zPageMail.zVerifyMailExists(subject);
 
