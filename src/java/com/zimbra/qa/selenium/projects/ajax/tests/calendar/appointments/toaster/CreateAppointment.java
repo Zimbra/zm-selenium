aae151b4b47850431f509d427ea32548fefc5cea
Jitesh Sojitra <jsojitra@zimbra.com>
2016-02-26 01:10:22 -0600

    (split)bug:104145,103255 - Fixed create appointment toast message issue especially for coverage
    https://bugzilla.zimbra.com/show_bug.cgi?id=104145
    https://bugzilla.zimbra.com/show_bug.cgi?id=103255
    
    Copied from Perforce
     Change: 566799

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/appointments/toaster/CreateAppointment.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/appointments/toaster/CreateAppointment.java
@@ -28,23 +28,18 @@ public class CreateAppointment extends AjaxCommonTest {
 
 	public CreateAppointment() {
 		logger.info("New "+ CreateAppointment.class.getCanonicalName());
-
-		// All tests start at the Calendar page
 		super.startingPage = app.zPageCalendar;
-
-		// Make sure we are using an account with message view
 		super.startingAccountPreferences = null;
 	}
 	
 	@Test(	description = "Verify Toaster message on Create Appointment",
-			groups = { "functional" }
-	)
+			groups = { "functional" } )
+	
 	public void Toaster_CreateAppointment_01() throws HarnessException {
 		
 		// Create the message data to be sent
 		AppointmentItem appt = new AppointmentItem();
 		appt.setSubject(ZimbraSeleniumProperties.getUniqueString());
-		appt.setContent("content" + ZimbraSeleniumProperties.getUniqueString());
 
 		// Open the new mail form
 		FormApptNew apptForm = (FormApptNew) app.zPageCalendar.zToolbarPressButton(Button.B_NEW);
@@ -54,13 +49,13 @@ public class CreateAppointment extends AjaxCommonTest {
 		apptForm.zFill(appt);
 
 		// Send the message
-		apptForm.zToolbarPressButton(Button.B_SAVE);
-			
+		// apptForm.zToolbarPressButton(Button.B_SAVE); // this method by default waits for some sec, toast message disappears so testcase fails
+		app.zPageCalendar.zClickAt("css=div[id^='ztb__APPT-'] td[id$='_SAVE_title']", "0,0");
+		
 		//verify toasted message 'Appointment Created'  
         String expectedMsg ="Appointment Created";
         ZAssert.assertStringContains(app.zPageMain.zGetToaster().zGetToastMessage(),expectedMsg, "Verify toast message '" + expectedMsg + "'");
     
-		
 	}
 
 }
