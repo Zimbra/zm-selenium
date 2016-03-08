e1024a238863e055f35b7d928376103656287f75
Ankit Kumar <kumara@zimbra.com>
2016-03-08 04:16:18 -0600

    (split)bug:103259
    -Added case for bug 88160 (checkerrorafterremovingattachment)
    -Added case for bug 77538 (deleremailifullviewpane)
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=103259
    
    Copied from Perforce
     Change: 567704

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/attachments/CheckErrorAfterRemovingAttachement.java
@@ -0,0 +1,140 @@
+
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.attachments;
+
+import java.io.File;
+import java.util.List;
+import org.testng.annotations.Test;
+import com.zimbra.common.soap.Element;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.*;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+
+public class CheckErrorAfterRemovingAttachement extends PrefGroupMailByMessageTest {
+
+	public CheckErrorAfterRemovingAttachement() throws HarnessException {
+		logger.info("New "+ CheckErrorAfterRemovingAttachement.class.getCanonicalName());
+
+	}
+
+	@Test(	description = "Bug 88160 - Remove an attachment from a mail and then delete mail", groups = { "functional" })
+
+	public void CheckErrorAfterRemovingAttachement_01() throws HarnessException {
+
+		// Data Setup	
+		final String mimeFile = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/mime/email05/mime01.txt";
+		final String subject = "subject151615738";
+		final String attachmentname = "file.txt";
+		ZimbraAccount account = app.zGetActiveAccount();
+
+		// Inject the message
+		LmtpInject.injectFile(app.zGetActiveAccount().EmailAddress, new File(mimeFile));
+
+		// Verification for attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+						+		"<query>subject:("+ subject +")</query>"
+						+	"</SearchRequest>");
+		String id = account.soapSelectValue("//mail:m", "id");
+
+		account.soapSend(
+				"<GetMsgRequest xmlns='urn:zimbraMail' >"
+						+		"<m id='"+ id +"'/>"
+						+	"</GetMsgRequest>");
+		Element[] nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+		ZAssert.assertGreaterThan(nodes.length, 0, "Verify the message has the attachment");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail display = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		AttachmentItem item = null;
+		List<AttachmentItem> items = display.zListGetAttachments();
+		for (AttachmentItem i : items) {
+			if ( i.getAttachmentName().equals(attachmentname)) {
+				item = i;
+				break;
+			}
+		}
+
+		ZAssert.assertNotNull(item, "Verify one attachment is in the message");
+
+		// Double click the message to open in full reading pane
+		app.zPageMail.zListItem(Action.A_DOUBLECLICK, subject);
+
+		// Click remove attachment
+		DialogWarning dialog = (DialogWarning)display.zListAttachmentItem(Button.B_REMOVE, item);
+		dialog.zClickButton(Button.B_YES);
+
+		//-- Verification
+		// Verify the message no longer has an attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+						+		"<query>subject:("+ subject +")</query>"
+						+	"</SearchRequest>");
+		id = account.soapSelectValue("//mail:m", "id");
+
+		try{
+
+			int i = 0;
+			do {
+				SleepUtil.sleepSmall();
+
+				account.soapSend(
+						"<GetMsgRequest xmlns='urn:zimbraMail' >"
+								+   "<m id='"+ id +"'/>"
+								+ "</GetMsgRequest>");
+				nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+
+			} while ( (i++ < 10) && (nodes.length > 0) );
+
+
+		}catch(Exception ex){
+			logger.error(ex);
+		}
+
+		ZAssert.assertEquals(nodes.length, 0, "Verify the message no longer has the attachment");
+
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+
+		// Select message and then click delete
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+		app.zPageMail.zToolbarPressButton(Button.B_DELETE);
+
+		// Verify the message no longer exist in the list
+		List<MailItem> messages = app.zPageMail.zListGetMessages();
+		ZAssert.assertNotNull(messages, "Verify the message list exists");
+
+		MailItem found = null;
+		for (MailItem m : messages) {
+			logger.info("Subject: looking for "+ mail.dSubject +"m  found: "+ m.gSubject);
+			if ( mail.dSubject.equals(m.gSubject) ) {
+				found = m;
+				break;
+			}
+		}
+		ZAssert.assertNull(found, "Verify the message is no longer in the inbox");
+
+
+	}
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mail/DeleteMailInFullReadingPane.java
@@ -0,0 +1,85 @@
+
+
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.mail;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+
+public class DeleteMailInFullReadingPane extends PrefGroupMailByMessageTest {
+
+	public DeleteMailInFullReadingPane() throws HarnessException {
+		logger.info("New "+ DeleteMailInFullReadingPane.class.getCanonicalName());
+
+	}
+
+	@Test(	description = "Bug 77538 - Double click a mail and delete it", groups = { "functional" })
+
+	public void DeleteMailInFullReadingPane_01() throws HarnessException {
+
+		// Data Setup	
+		String subject = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+						"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<su>"+ subject +"</su>" +
+						"<mp ct='text/plain'>" +
+						"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+						"</m>" +
+				"</SendMsgRequest>");
+
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Double click the message to open in full reading pane
+		app.zPageMail.zListItem(Action.A_DOUBLECLICK, subject);
+
+		// click delete
+		//app.zPageMail.zClick("zDeleteButtonFullViewPane"); //Delete button locator
+		//SleepUtil.sleepMedium();
+
+		app.zPageMail.zToolbarPressButtonFullView(Button.B_DELETE_FULL_VIEW_PANE);
+		
+		// Verify the message no longer exist in the list
+		List<MailItem> messages = app.zPageMail.zListGetMessages();
+		ZAssert.assertNotNull(messages, "Verify the message list exists");
+
+		MailItem found = null;
+		for (MailItem m : messages) {
+			logger.info("Subject: looking for "+ mail.dSubject +" found: "+ m.gSubject);
+			if ( mail.dSubject.equals(m.gSubject) ) {
+				found = m;
+				break;
+			}
+		}
+		ZAssert.assertNull(found, "Verify the message is no longer in the inbox");
+
+		ZAssert.assertFalse(app.zPageMail.zIsVisiblePerPosition("css=div[id^='ztb__MSG'] div[id$='DELETE'] tr td[id$='DELETE_title']", 10, 10), "Verify delete button is not present");
+	}
+}
