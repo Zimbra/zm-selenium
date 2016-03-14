2dd535d0069ca72e47c2a90027f4630027088dbd
Girish Bhamare <girish@zimbra.com>
2016-03-14 01:34:42 -0500

    (split)bug:103256
    --Added test cases for unflag mail from new window.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=103256
    
    Copied from Perforce
     Change: 568139

---
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/newwindow/mail/UnFlagMail.java
@@ -0,0 +1,206 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.newwindow.mail;
+
+
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.SeparateWindowDisplayMail;
+
+public class UnFlagMail extends PrefGroupMailByMessageTest {
+
+	public int delaySeconds = 10;
+
+	public UnFlagMail() {
+		logger.info("New " + UnFlagMail.class.getCanonicalName());
+
+		super.startingAccountPreferences.put("zimbraPrefMarkMsgRead", ""
+				+ delaySeconds);
+
+	}
+
+
+	@Test(description = "Un Flag from new window ,action menu -> UnFlag", groups = { "functional" })
+	public void UnFlagrFromNewWindow_01() throws HarnessException {
+
+		// Create the message data to be sent
+		String subject = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+
+		FolderItem inboxFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>" + "<m l='"
+						+ inboxFolder.getId() + "' f='f'>"
+						+ "<content>From: foo@foo.com\n" + "To: foo@foo.com \n"
+						+ "Subject: " + subject + "\n" + "MIME-Version: 1.0 \n"
+						+ "Content-Type: text/plain; charset=utf-8 \n"
+						+ "Content-Transfer-Encoding: 7bit\n" + "\n"
+						+ "simple text string in the body\n" + "</content>"
+						+ "</m>" + "</AddMsgRequest>");
+
+		// Create a mail item to represent the message
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+		ZAssert.assertStringContains(mail.getFlags(), "f", "Verify message is initially flagged");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+
+		SeparateWindowDisplayMail window = null;
+
+		try {
+
+			// Choose Actions -> Launch in Window
+			window = (SeparateWindowDisplayMail) app.zPageMail.zToolbarPressPulldown(Button.B_ACTIONS,Button.B_LAUNCH_IN_SEPARATE_WINDOW);
+
+			window.zSetWindowTitle(subject);
+			window.zWaitForActive(); // Make sure the window is there
+
+			ZAssert.assertTrue(window.zIsActive(),"Verify the window is active");
+
+			//Actin Menu --> Flag
+			window.zToolbarPressPulldown(Button.B_ACTIONS,Button.B_UNFLAG_MESSAGE);
+
+			SleepUtil.sleepMedium();
+
+		} finally {
+
+			// Make sure to close the window
+			if (window != null) {
+				window.zCloseWindow();
+				window = null;
+			}
+
+		}
+
+		//	MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(),"subject:(" + subject + ")");
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+
+		List<MailItem> messages = app.zPageMail.zListGetMessages();
+		ZAssert.assertNotNull(messages, "Verify the message list exists");
+
+		MailItem listmail = null;
+		for (MailItem m : messages) {
+			logger.info("Subject: looking for " + mail.dSubject + " found: "
+					+ m.gSubject);
+			if (mail.dSubject.equals(m.gSubject)) {
+				listmail = m;
+				break;
+			}
+		}
+
+		// Make sure the GUI shows "flagged"
+		ZAssert.assertNotNull(listmail, "Verify the message is in the list");
+		ZAssert.assertFalse(listmail.gIsFlagged,"Verify the message is flagged in the list");
+
+		// Make sure the server shows "flagged"
+		mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject + ")");
+		ZAssert.assertStringDoesNotContain(mail.getFlags(), "f","Verify the message is flagged in the server");
+
+	}
+
+	@Test(description = "Un Flag mail from new window ,using shortcut 'mf'", groups = { "functional" })
+	public void UnFlagrFromNewWindow_02() throws HarnessException {
+
+		// Create the message data to be sent
+		String subject = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+
+		FolderItem inboxFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>" + "<m l='"
+						+ inboxFolder.getId() + "' f='f'>"
+						+ "<content>From: foo@foo.com\n" + "To: foo@foo.com \n"
+						+ "Subject: " + subject + "\n" + "MIME-Version: 1.0 \n"
+						+ "Content-Type: text/plain; charset=utf-8 \n"
+						+ "Content-Transfer-Encoding: 7bit\n" + "\n"
+						+ "simple text string in the body\n" + "</content>"
+						+ "</m>" + "</AddMsgRequest>");
+
+		// Create a mail item to represent the message
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+		ZAssert.assertStringContains(mail.getFlags(), "f", "Verify message is initially flagged");
+
+		// Refresh current view
+		app.zPageMail.zVerifyMailExists(subject);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+
+		SeparateWindowDisplayMail window = null;
+
+		try {
+
+			// Choose Actions -> Launch in Window
+			window = (SeparateWindowDisplayMail) app.zPageMail.zToolbarPressPulldown(Button.B_ACTIONS,Button.B_LAUNCH_IN_SEPARATE_WINDOW);
+
+			window.zSetWindowTitle(subject);
+			window.zWaitForActive(); // Make sure the window is there
+
+			ZAssert.assertTrue(window.zIsActive(),"Verify the window is active");
+
+			// UnFlag the item
+			app.zPageMail.zKeyboardShortcut(Shortcut.S_MAIL_MARKFLAG);
+
+			SleepUtil.sleepMedium();
+
+		} finally {
+
+			// Make sure to close the window
+			if (window != null) {
+				window.zCloseWindow();
+				window = null;
+			}
+
+		}
+
+		//	MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(),"subject:(" + subject + ")");
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+
+		List<MailItem> messages = app.zPageMail.zListGetMessages();
+		ZAssert.assertNotNull(messages, "Verify the message list exists");
+
+		MailItem listmail = null;
+		for (MailItem m : messages) {
+			logger.info("Subject: looking for " + mail.dSubject + " found: "
+					+ m.gSubject);
+			if (mail.dSubject.equals(m.gSubject)) {
+				listmail = m;
+				break;
+			}
+		}
+
+		// Make sure the GUI shows "flagged"
+		ZAssert.assertNotNull(listmail, "Verify the message is in the list");
+		ZAssert.assertFalse(listmail.gIsFlagged,"Verify the message is flagged in the list");
+
+		// Make sure the server shows "flagged"
+		mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject + ")");
+		ZAssert.assertStringDoesNotContain(mail.getFlags(), "f","Verify the message is flagged in the server");
+	}
+
+}
+
