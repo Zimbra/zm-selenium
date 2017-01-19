21e1730f005e23938055f873b8cbf84464e2699b
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2017-01-19 13:08:46 +0530

    Fixed group for one of the task testcase

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/GetTask.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/GetTask.java
@@ -397,7 +397,7 @@ public class GetTask extends AjaxCommonTest {
 
 	@Bugs(ids="64681,72236")
 	@Test( description = "No refresh after task is marked complete in filter to-do list",
-			groups = { "smoke", "L1"})
+			groups = { "functional", "L3"})
 
 	public void GetTask_07() throws HarnessException {
 		
