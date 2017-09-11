f1f4de35aa816430713fa6537dea0db454d75d99
Mitesh Savani <mitesh.savani@synacor.com>
2017-09-11 18:57:47 +0530

    Resolve conflict

--- src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/recurring/modify/ModifyInstanceModifySeries.java
+++ src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/recurring/modify/ModifyInstanceModifySeries.java
@@ -213,6 +213,7 @@ public class ModifyInstanceModifySeries extends CalendarWorkWeekTest {
 		apptForm.zFillField(Field.Body, modifiedSecondInstanceBody);
 		apptForm.zFill(appt);
         apptForm.zToolbarPressButton(Button.B_SEND);
+        
 	
 		app.zPageCalendar.zToolbarPressButton(Button.B_NEXT_PAGE);
 		app.zPageCalendar.zToolbarPressButton(Button.B_NEXT_PAGE);
@@ -229,6 +230,7 @@ public class ModifyInstanceModifySeries extends CalendarWorkWeekTest {
 		apptForm.zFillField(Field.Body, modifiedSixthInstanceBody);
 		apptForm.zFill(appt);
         apptForm.zToolbarPressButton(Button.B_SEND);
+        
 		
 		app.zPageCalendar.zToolbarPressButton(Button.B_NEXT_PAGE);
 		openRecurring = (DialogOpenRecurringItem) app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
@@ -237,8 +239,9 @@ public class ModifyInstanceModifySeries extends CalendarWorkWeekTest {
 		
 		apptForm = new FormApptNew(app);	
 		apptForm.zRepeat(Button.O_EVERY_WEEK_MENU, Button.B_EVERY_X_RADIO_BUTTON, "", Button.B_END_BY_DATE_RADIO_BUTTON, "01/01/2020");
-
 		apptForm.zToolbarPressButton(Button.B_SEND);
+
+        SleepUtil.sleepMedium();
         
 		app.zGetActiveAccount().soapSend(
 				"<SearchRequest xmlns='urn:zimbraMail' types='appointment' calExpandInstStart='"+ startUTC.addDays(-10).toMillis() +"' calExpandInstEnd='"+ endUTC.addDays(10).toMillis() +"'>"
