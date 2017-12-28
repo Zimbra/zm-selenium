091639c9943fdb034cd1d2a2e6754a393e6dae68
Shubham Gupta <shubham.gupta@synacor.com>
2017-12-28 18:07:57 +0530

    ZCS-3854: Added L0 testcases for zextras HSM module

---
+++ src/java/com/zimbra/qa/selenium/projects/admin/pages/PageZextrasHSM.java
@@ -0,0 +1,154 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.admin.pages;
+
+import com.zimbra.qa.selenium.framework.ui.AbsApplication;
+import com.zimbra.qa.selenium.framework.ui.AbsPage;
+import com.zimbra.qa.selenium.framework.ui.AbsTab;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+
+public class PageZextrasHSM extends AbsTab {
+
+	public static class Locators {
+		public static final String NETWORK_MODULE_NG_ICON = "css=td[id^='zti__AppAdmin__Home__ZeXtras_'] div[class='ImgZeXtras']";
+		public static final String HSM_TAB = "css=td[id^='zti__AppAdmin__ZeXtras__ZxPowerstore_']:contains('HSM')";
+		public static final String CLOSE_BUTTON = "css=td[id^='zb__ZaCurrentAppBar__CLOSE_']:contains('Close')";
+		public static final String SAVE_BUTTON = "css=td[id^='zb__ZaCurrentAppBar__SAVE_']:contains('Save')";
+		public static final String APPLY_HSM_POLICY_NOW_BUTTON = "css=td[class='ZWidgetTitle']:contains('Apply HSM Policy Now')";
+		public static final String SECONDARY_VOL_ADD_BUTTON = "css=div[id='ztabv__ZxPowerstore_zawiz_top_grouper_2'] td[class='ZWidgetTitle']:contains('Add')";
+		public static final String SECONDARY_VOL_EDIT_BUTTON = "css=div[id='ztabv__ZxPowerstore_zawiz_top_grouper_2'] td[class='ZWidgetTitle']:contains('Edit')";
+		public static final String SECONDARY_VOL_DELETE_BUTTON = "css=div[id='ztabv__ZxPowerstore_zawiz_top_grouper_2'] td[class='ZWidgetTitle']:contains('Delete')";
+		public static final String HSM_POLICY_ADD_BUTTON = "css=div[id='ztabv__ZxPowerstore_group_14'] td[class='ZWidgetTitle']:contains('Add')";
+		public static final String HSM_POLICY_EDIT_BUTTON = "css=div[id='ztabv__ZxPowerstore_group_14'] td[class='ZWidgetTitle']:contains('Edit')";
+		public static final String HSM_POLICY_DELETE_BUTTON = "css=div[id='ztabv__ZxPowerstore_group_14'] td[class='ZWidgetTitle']:contains('Delete')";
+		public static final String VOL_ADD_EDIT_OK_BUTTON = "css=div#zdlg__MSG.DwtDialog[style*='z-index: 7'] td[class='ZWidgetTitle']:contains('OK')";
+		public static final String lastPolicyInList = "css=div.hsmList div[class='DwtListView-Rows'] div:last-child";
+	}
+
+	public PageZextrasHSM(AbsApplication application) {
+		super(application);
+
+		logger.info("new " + myPageName());
+	}
+
+	@Override
+	public String myPageName() {
+		return (this.getClass().getName());
+	}
+
+	@Override
+	public boolean zIsActive() throws HarnessException {
+
+		// Make sure the Admin Console is loaded in the browser
+		if (!MyApplication.zIsLoaded())
+			throw new HarnessException("Admin Console application is not active!");
+
+		boolean present = sIsElementPresent(Locators.CLOSE_BUTTON);
+		if (!present) {
+			return (false);
+		}
+
+		boolean visible = zIsVisiblePerPosition(Locators.CLOSE_BUTTON, 0, 0);
+		if (!visible) {
+			logger.debug("isActive() visible = " + visible);
+			return (false);
+		}
+		return (true);
+	}
+
+	@Override
+	public void zNavigateTo() throws HarnessException {
+
+		if (zIsActive()) {
+			return;
+		}
+
+		SleepUtil.sleepLong();
+		sClickAt(Locators.NETWORK_MODULE_NG_ICON, "");
+		zWaitForWorkInProgressDialogInVisible();
+		sClickAt(Locators.HSM_TAB, "");
+		zWaitForWorkInProgressDialogInVisible();
+	}
+
+	@Override
+	public AbsPage zListItem(Action action, String accountEmailAddress) throws HarnessException {
+
+		return null;
+	}
+
+	@Override
+	public AbsPage zListItem(Action action, Button option, String accountEmailAddress) throws HarnessException {
+		return null;
+	}
+
+	@Override
+	public AbsPage zListItem(Action action, Button option, Button subOption, String item) throws HarnessException {
+		return null;
+	}
+
+	@Override
+	public AbsPage zToolbarPressButton(Button button) throws HarnessException {
+		return null;
+	}
+
+	@Override
+	public AbsPage zToolbarPressPulldown(Button pulldown, Button option) throws HarnessException {
+		return null;
+	}
+
+	public boolean zVerifyHeader(String header) throws HarnessException {
+		if (this.sIsElementPresent("css=span:contains('" + header + "')"))
+			return true;
+		return false;
+	}
+
+	public void zSave() throws HarnessException {
+		SleepUtil.sleepSmall();
+		sClickAt(Locators.SAVE_BUTTON, "");
+		SleepUtil.sleepLong();
+	}
+
+	public AbsPage AddSecondaryVol() throws HarnessException {
+		AbsPage page = null;
+		SleepUtil.sleepVerySmall();
+		page = new WizardAddNewVolume(this);
+		this.sClickAt(Locators.SECONDARY_VOL_ADD_BUTTON, "");
+		return (page);
+	}
+	public AbsPage AddHSMPolicy() throws HarnessException {
+		AbsPage page = null;
+		SleepUtil.sleepVerySmall();
+		page = new WizardHSMPolicy(this);
+		this.sClickAt(Locators.HSM_POLICY_ADD_BUTTON, "");
+		SleepUtil.sleepVerySmall();
+		return (page);
+	}
+	public Boolean IsSecVolAdded(String volName) throws HarnessException {
+		return(this.sIsElementPresent("css=div.secondaryVolumes div.DwtListView-Rows div:contains('"+ volName +"')"));
+	}
+
+	public Boolean IsCurrentVolume(String volName) throws HarnessException {
+		return(this.sIsElementPresent("xpath=//div[contains(text(),'Secondary Volumes')]/parent::div//td[contains(text(),'"+volName+"')]/preceding-sibling::td[2]/div[@class='ImgCheck']"));
+	}
+	
+	public String GetLastHSMPolicyInList() throws HarnessException {
+		return(sGetText(Locators.lastPolicyInList));
+	}
+}
---
+++ src/java/com/zimbra/qa/selenium/projects/admin/pages/WizardHSMPolicy.java
@@ -0,0 +1,121 @@
+package com.zimbra.qa.selenium.projects.admin.pages;
+
+import com.zimbra.qa.selenium.framework.items.IItem;
+import com.zimbra.qa.selenium.framework.ui.AbsTab;
+import com.zimbra.qa.selenium.framework.ui.AbsWizard;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.projects.admin.items.HSMPolicyItem;
+
+public class WizardHSMPolicy extends AbsWizard {
+
+	public WizardHSMPolicy(AbsTab page) {
+		super(page);
+	}
+	
+	public static class Locators {
+		public static final String addNewHSMPolicyDialog = "css=div[class='DwtDialog']:not([aria-hidden='true'])";
+		public static final String emailsItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_3']";
+		public static final String documentsItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_4']";
+		public static final String tasksItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_5']";
+		public static final String appointmentsItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_6']";
+		public static final String contactsItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_7']";
+		public static final String chatsItemCheckbox = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmType_8']";
+		public static final String olderThan = "css=div[class='DwtDialog']:not([aria-hidden='true']) input[id$='_hsmQuery_3']";
+		public static final String pullDown = "css=div[class='DwtDialog']:not([aria-hidden='true']) div.ImgSelectPullDownArrow";
+		public static final String okButton = "css=div[class='DwtDialog']:not([aria-hidden='true']) td[class='ZWidgetTitle']:contains('OK')";
+		public static final String successfulDialog = "css=div.DwtDialog[style*='display: block;'] table td:contains('Zimbra Administration')";
+		
+		//public static final String duration = "css=div[class='oselect_choice']:contains('minutes')";
+	}
+	
+	@Override
+	public IItem zCompleteWizard(IItem item) throws HarnessException {
+		
+		if(!(item instanceof HSMPolicyItem))
+			throw new HarnessException("item must be an HSMPolicyItem, was " + item.getClass().getCanonicalName());
+		
+		HSMPolicyItem policy = (HSMPolicyItem)item;
+		
+		if(policy.getEmailItem() != null) {
+			if(policy.getEmailItem()){
+				sCheck(Locators.emailsItemCheckbox);
+			}else {
+				sUncheck(Locators.emailsItemCheckbox);
+			}
+		}
+		if(policy.getDocumentItem() != null){
+			if(policy.getEmailItem()){
+				sCheck(Locators.documentsItemCheckbox);
+			}else {
+				sUncheck(Locators.documentsItemCheckbox);
+			}
+		}
+		if(policy.getTasksItem() != null){
+			if(policy.getEmailItem()){
+				sCheck(Locators.tasksItemCheckbox);
+			}else {
+				sUncheck(Locators.tasksItemCheckbox);
+			}
+		}
+		if(policy.getAppointmentItem() != null){
+			if(policy.getEmailItem()){
+				sCheck(Locators.appointmentsItemCheckbox);
+			}else {
+				sUncheck(Locators.appointmentsItemCheckbox);
+			}
+		}
+		if(policy.getContactsItem() != null){
+			if(policy.getEmailItem()){
+				sCheck(Locators.contactsItemCheckbox);
+			}else {
+				sUncheck(Locators.contactsItemCheckbox);
+			}
+		}
+		if(policy.getChatsItem() != null){
+			if(policy.getEmailItem()){
+				sCheck(Locators.chatsItemCheckbox);
+			}else {
+				sUncheck(Locators.chatsItemCheckbox);
+			}
+		}
+		
+		SleepUtil.sleepVerySmall();
+		sType(Locators.olderThan, policy.getOlderThan());
+		sClickAt(Locators.pullDown, "");
+		sClickAt("css=div[class='oselect_choice']:contains('"+policy.getTimeDuration()+"')", "");
+		sClickAt(Locators.okButton, "");
+		
+		if(zWaitForElementPresent(Locators.successfulDialog)) {
+			sClick(Locators.okButton);
+		} else {
+			throw new HarnessException("Policy added successfully dialog is not appeared");
+		}
+		
+		return null;
+	}
+
+	@Override
+	public String myPageName() {
+		return (this.getClass().getName());
+	}
+
+	@Override
+	public boolean zIsActive() throws HarnessException {
+		logger.info(myPageName() + " zIsActive()");
+
+		String locator = Locators.addNewHSMPolicyDialog;
+
+		if (!this.sIsElementPresent(locator)) {
+			return (false);
+		}
+
+		if (!this.zIsVisiblePerPosition(locator, 0, 0)) {
+			return (false);
+		}
+
+		logger.info(myPageName() + " zIsActive() = true");
+		return (true);
+	}
+
+}
