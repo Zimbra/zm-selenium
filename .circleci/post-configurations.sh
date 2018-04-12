3dbfcba92870f261e74f582d3a973e8e9774326e
Jitesh Sojitra <jitesh.sojitra@synacor.com>
2018-04-12 14:43:27 +0530

    ZCS-4794: Rename for html report symbolic name [No review required]

--- .circleci/post-configurations.sh
+++ .circleci/post-configurations.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 # Create test output links
-ln -s ~/zm-selenium/test-output/*/*/*/*/TestNG/index.html ~/zm-selenium/test-output/report.html
+ln -s ~/zm-selenium/test-output/*/*/*/*/TestNG/index.html ~/zm-selenium/test-output/testng-report.html
 ln -s ~/zm-selenium/test-output/*/*/*/*/debug/projects/harness.log ~/zm-selenium/test-output
